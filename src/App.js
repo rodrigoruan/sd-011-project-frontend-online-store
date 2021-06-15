55f3860da1a8d5339c3a69c8959a965d14eec185
Júlio Mota <juliomotacampos@gmail.com>
2021-06-15 16:55:32 -0300

    Adiciona requisito 1

--- src/App.js
+++ src/App.js
@@ -1,27 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+// import * as api from './services/api';
 
-function App() {
-  // Iniciando o projeto;
-  // Júlio chegou aqui.
-  // A wil Robson appears
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    // api.getCategories().then(categories => { console.log(categories) });
+    // api.getProductsFromCategoryAndQuery('MLB1055', 'Motorola').then(categories => { console.log(categories) });
+
+    return (
+      <div> textInComponent </div>
+    );
+  }
 }
 
 export default App;
