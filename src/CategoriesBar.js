f4ac889ec317bd2b64f7210c4fac73b60b131d6c
fernando lasmar <f3rnandolasmar@gmail.com>
2021-06-15 18:36:22 -0300

    requisito 4

---
+++ src/CategoriesBar.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import * as api from './services/api';
+import Loading from './Loading';
+
+class CategoriesBar extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      categories: [],
+      loading: true,
+    };
+    this.handleFetch = this.handleFetch.bind(this);
+    this.renderCategorie = this.renderCategorie.bind(this);
+  }
+
+  componentDidMount() {
+    this.handleFetch();
+  }
+
+  async handleFetch() {
+    this.setState({
+      loading: true,
+
+    }, async () => {
+      const reponse = await api.getCategories();
+      this.setState({
+        loading: false,
+        categories: reponse,
+      });
+    });
+  }
+
+  renderCategorie() {
+    const { categories } = this.state;
+    return (
+      <ul>
+        {
+          categories.map((category) => (
+            <li data-testid="category" key={ category.id }>
+              {category.name}
+            </li>
+          ))
+        }
+      </ul>
+    );
+  }
+
+  render() {
+    const { loading } = this.state;
+
+    return (
+      <div>
+        <h1>Categorias</h1>
+        { loading ? <Loading /> : this.renderCategorie() }
+      </div>
+    );
+  }
+}
+
+export default CategoriesBar;
--- src/home.js
+++ src/home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import CategoriesBar from './CategoriesBar';
 
 class Home extends React.Component {
   render() {
@@ -9,6 +10,7 @@ class Home extends React.Component {
         >
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+        <CategoriesBar />
       </div>
     );
   }
