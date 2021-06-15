0576c4b5cc8860330fae8c9364e871aa2548c978
Leandro Lopes <leandro_carlos@uol.com.br>
2021-06-15 18:03:15 -0300

    adiciona requisito 3

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import carrinho from '../carrinho.png';
 
 class LandingPage extends React.Component {
   render() {
     return (
       <div data-testid="home-initial-message">
         <p>Digite algum termo de pesquisa ou escolha uma categoria.</p>
+        <Link data-testid="shopping-cart-button" to="/shopping-cart">
+          <img src={ carrinho } alt="carrinho" />
+        </Link>
       </div>
     );
   }
