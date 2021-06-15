2c77c714e5df291b1966acdb1fb6c1d5847b839c
Gisele Costa <jikinha@gmail.com>
2021-06-15 16:48:11 -0300

    Requerimento 3 pronto

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Home extends Component {
   render() {
     return (
       <div>
+        <Link to="/cart" data-testid="shopping-cart-button">Carrinho</Link>
         <h2 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h2>
