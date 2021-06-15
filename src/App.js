e0a94c007315bd29731a9922fe20d5f3be43272a
Lucas Maestrelli <lucasmaestrelli95@gmail.com>
2021-06-15 16:23:35 -0300

    requisito 3

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './Pages/Home';
+import Shopcart from './Pages/Shopcart';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Switch>
+          <Route path="/shopcart" component={ Shopcart } />
           <Route path="/" component={ Home } />
         </Switch>
       </Router>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Home extends Component {
   constructor(props) {
@@ -14,6 +15,7 @@ export default class Home extends Component {
         <h1 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h1>
+        <Link data-testid='shopping-cart-button'to="/shopcart">Carrinho</Link>
       </div>
     );
   }
