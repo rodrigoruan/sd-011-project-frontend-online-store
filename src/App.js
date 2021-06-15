a5e7439e7b3d1125ff68d8463d196c2331c124c9
Taís Carvalho <taispcarvalho@gmail.com>
2021-06-15 19:11:46 -0300

    requisito3

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
 import Home from './Pages/Home';
+import ShoppingCartButton from './Pages/ShoppingCartButton';
+import ShoppingCart from './Pages/ShoppingCart';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <Home />
+        <ShoppingCartButton />
+        <ShoppingCart />
       </div>);
   }
 }
