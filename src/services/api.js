a6c97ed24688a0b00f9b7162f29a7995399c9f32
KaioHPs <kaiohpsalternativo@gmail.com>
2021-06-15 16:11:01 -0300

    Requisito 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,19 @@
 export async function getCategories() {
   // Implemente aqui
+   return fetch('https://api.mercadolibre.com/sites/MLB/categories')
+    .then((categories) => categories.json());
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
+export async function getProductsFromCategoryAndQuery( categoryId, query) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+  if (categoryId && !query) {
+    return fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`)
+    .then((products) => products.json());
+  } else if (!categoryId && query) {
+    return fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`)
+    .then((products) => products.json());
+  } else if (categoryId && query) {
+    return fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}_ID&q=${query}`)
+    .then((products) => products.json());
+  }
 }
