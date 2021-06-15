284b7928a8451a91a5c931e85fbea26bb07f53e3
Matheus Antonio da Silva <matheusantonio232@gmail.com>
2021-06-15 18:13:03 -0300

    Requisito 3 parte 1

---
+++ src/Components/ButtonShopCart.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ButtonShopCart extends Component {
+  render() {
+    return (
+      <button data-testid="shopping-cart-button" type="button">
+        Adicionar ao Carrinho
+      </button>
+    );
+  }
+}
+
+export default ButtonShopCart;
