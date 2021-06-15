56a7a70849824215bc9ae3a3b9bd3a8bd929dd2f
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-06-15 17:55:13 -0300

    Finalizado requisito 2

---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import ProductSearch from '../components/ProductSearch';
+
+class Home extends Component {
+  render() {
+    return (
+      <ProductSearch />
+    );
+  }
+}
+export default Home;
