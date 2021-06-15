6106e2978136e895d48bab3fc9a00de61f23c0c3
JulioCesar1402 <julio.cesarcj1402@gmail.com>
2021-06-15 18:56:32 -0300

    correçao lint

--- src/pages/CategoryFilter.js
+++ src/pages/CategoryFilter.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { getCategories } from '../services/api'
+import { getCategories } from '../services/api';
 
 export default class CategoryFilter extends Component {
   constructor() {
@@ -7,24 +7,28 @@ export default class CategoryFilter extends Component {
 
     this.state = {
       categories: [],
-    }
+    };
+  }
+
+  componentDidMount() {
+    this.fetchFilterCategories();
   }
 
   async fetchFilterCategories() {
     const categories = await getCategories();
     this.setState({ categories });
   }
-  
-  componentDidMount() {
-   this.fetchFilterCategories();
-  }
 
   render() {
-    const value = this.state.categories;
+    const { categories } = this.state;
     return (
       <ul>
         {
-          value.map((cat) => <li data-testid="category" key={ cat.id }> { cat.name } </li>)
+          categories.map((cat) => (
+            <li data-testid="category" key={ cat.id }>
+              { cat.name }
+            </li>
+          ))
         }
       </ul>
     );
