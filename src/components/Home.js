4a5ab0f22f50aa6da94f1b8f65b87448ecbcd54a
Oryange <oryangestrifezze@gmail.com>
2021-06-15 16:49:38 -0300

    Adiciona o Requisito 3

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Home extends Component {
   render() {
     return (
       <div>
         <input type="text" placeholder="digite aqui" />
+        <Link data-testid="shopping-cart-button" to="cart">Carrinho</Link>
         <p
           data-testid="home-initial-message"
         >
