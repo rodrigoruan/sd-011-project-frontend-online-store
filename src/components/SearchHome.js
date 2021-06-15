d2e0e4b095ad06642aeaee335d83d99ea50fb77a
Rodolfo <rodolforrcont@gmail.com>
2021-06-15 17:50:24 -0300

    Listandoe as categorias de produtos disponíveis via API na página principal

--- src/components/SearchHome.js
+++ src/components/SearchHome.js
@@ -1,10 +1,38 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import * as AppServices from '../services/api';
+import Categories from './Categories';
 
 export default class SearchHome extends Component {
+  constructor() {
+    super();
+    this.state = {
+      categories: [],
+      loading: true,
+    };
+  }
+
+  componentDidMount() {
+    this.handleCategories();
+  }
+
+  async handleCategories() {
+    const results = await AppServices.getCategories();
+    this.setState({
+      categories: results,
+      loading: false,
+    });
+  }
+
   render() {
+    const { categories, loading } = this.state;
+    console.log(categories);
     return (
       <div data-testid="home-initial-message">
+        { !loading
+          && categories.map(
+            (category, index) => <Categories value={ category.name } key={ index } />,
+          ) }
         <label htmlFor="search">
           <input type="text" name="search" />
         </label>
