a335af34b557d01feddfa3c5be795f58ec09209e
Alexandre <naitwa.alexandre@gmail.com>
2021-06-15 17:15:43 -0300

    03 adicionado page cart e botao tela incial

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 
 class Home extends React.Component {
   render() {
     return (
       <div>
+        <Link to="/cart" data-testid="shopping-cart-button"><ShoppingCartIcon /></Link>
         <p
           data-testid="home-initial-message"
         >
