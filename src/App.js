caefe6f8f673b6fae0cc8ce18568d91c5bb2d6c3
Laura Gusmao <laurargus@gmail.com>
2021-06-15 18:18:39 -0300

    Adiciona requisito 2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
 import './App.css';
+// import * as api from './services/api';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React!!
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  // api.getCategories().then((categories) => { console.log(categories); });
+  // api.getProductsFromCategoryAndQuery('MLB5672').then((categories) => { console.log(categories); });
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Home } />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
