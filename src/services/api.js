59891e08c9ac234e0c4658f8f50559bf78f8a7cf
Oryange <oryangestrifezze@gmail.com>
2021-06-15 16:15:44 -0300

    Adiciona Requisito 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,18 @@
 export async function getCategories() {
-  // Implemente aqui
+  const endpoint = 'https://api.mercadolibre.com/sites/MLB/categories';
+  const response = await fetch(endpoint);
+  return response.json();
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  let endpoint;
+  if (categoryId) {
+    endpoint = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`;
+  }
+  if (categoryId && query) {
+    endpoint = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
+  }
+
+  const response = await fetch(endpoint);
+  return response.json();
 }
