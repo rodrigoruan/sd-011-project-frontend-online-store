414995ed0c07ca96397c416b78d0454ce227fa46
Thyago Pessoa <flash_thyago@hotmail.com>
2021-06-15 16:57:18 -0300

    Requisito 2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import MainPage from './components/MainPage';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Router>
+      <div className="App">
+        <h1>Hello World</h1>
+
+        <Switch>
+          <Route path="/" component={ MainPage } />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
