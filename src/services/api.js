5797fb77f8e9253dfd990b614d3126e3c9768d77
Gustavo Lemes <gustavolemes997@gmail.com>
2021-06-15 16:59:08 -0300

    Requisito 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 export async function getCategories() {
-
+  return fetch('https://api.mercadolibre.com/sites/MLB/categories').then(
+    (resolve) => resolve.json(),
+  );
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  return fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}_ID&q=${query}`).then((resolve) => resolve.json());
 }
