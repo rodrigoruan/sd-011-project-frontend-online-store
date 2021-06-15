5262ff6b933ba5a05a0e3dd1499a7def3c2ffb51
gabrielfndz <gabriel.omercador@hotmail.com>
2021-06-15 18:32:08 -0300

    req4 - Lista de Categorias

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
+import CategoryList from './CategoryList';
 
 export default class Home extends Component {
   render() {
     return (
-      <>
+      <div>
+        <div>
+          <CategoryList />
+        </div>
         <input type="text" />
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
-      </>
+      </div>
     );
   }
 }
