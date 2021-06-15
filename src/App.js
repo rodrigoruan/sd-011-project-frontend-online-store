4bed3d026692f0c2b3e68dbcb493a87af51fd6dd
marcoswarmling <marcoswarmling15@gmail.com>
2021-06-15 18:24:46 -0300

    g

--- src/App.js
+++ src/App.js
@@ -2,28 +2,22 @@ import React from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './home';
 import './App.css';
-import './services/api'
-import listproductterms from './components/listproductterms'
-// import * as api from './services/api'
+import Listproductterms from './components/listProductTerms'
+import * as api from './services/api'
 
 function App() {
   // api.getCategories().then(categories => { console.log(categories) })
   // api.getProductsFromCategoryAndQuery('MLB5672', 'Farol').then(categories => { console.log(categories) })
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={ logo } className="App-logo" alt="logo" />
-        <p>Edit src/App.js and save to reload.</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        
-        </a>
-      </header>
+    <div>
+    <div>
+    <Listproductterms />
+    </div>
+    <BrowserRouter>
+      <Route>
+        <Home path="/" />
+      </Route>
+    </BrowserRouter>
     </div>
   );
 }
