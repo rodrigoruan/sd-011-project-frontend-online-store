e6574d54930cbf98bcd84b0a666505527352259d
Víctor Faria <vitilevi@hotmail.com>
2021-06-15 16:12:10 -0300

    Feito requisito 1

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,11 @@
 export async function getCategories() {
-  // Implemente aqui
+  const requestApi = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
+  const data = await requestApi.json();
+  return data;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery(categoryId, query) {
+  const requestApi = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+  const data = await requestApi.json();
+  return data;
 }
