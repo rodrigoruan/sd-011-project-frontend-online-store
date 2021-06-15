5e1c4daa453581dd428a29d0e2696796a0ee1496
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-06-15 17:55:52 -0300

    Cria a página do carrinho de compras

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import * as api from './services/api';
 import Home from './components/Home';
+import ShoppingCart from './components/ShoppingCart';
 
 class App extends React.Component {
   render() {
@@ -10,6 +11,7 @@ class App extends React.Component {
       <BrowserRouter>
         <Switch>
           <Route exact path="/" component={ Home } api={ api } />
+          <Route exact path="/carrinho" component={ ShoppingCart } />
         </Switch>
       </BrowserRouter>
     );
