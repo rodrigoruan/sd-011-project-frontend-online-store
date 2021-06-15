e9632eb3b3cc098b123f829bf596c2a87b6ea5e7
Alexandre <naitwa.alexandre@gmail.com>
2021-06-15 17:24:24 -0300

    material ui bugado

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 
 class Home extends React.Component {
   render() {
     return (
       <div>
-        <Link to="/cart" data-testid="shopping-cart-button"><ShoppingCartIcon /></Link>
+        <Link to="/cart" data-testid="shopping-cart-button">Cart</Link>
         <p
           data-testid="home-initial-message"
         >
