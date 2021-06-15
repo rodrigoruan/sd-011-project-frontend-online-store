5c76f873dbb64dcd3f1c764ef2f1bbe1a6576289
Guilherme Pereira de Oliveira <guilhermepdo1508@gmail.com>
2021-06-15 16:23:09 -0300

    Requisito 2 feito

---
+++ src/components/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class LandingPage extends React.Component {
+  render() {
+    return (
+      <div data-testid="home-initial-message">
+        <p>Digite algum termo de pesquisa ou escolha uma categoria.</p>
+      </div>
+    );
+  }
+}
+
+export default LandingPage;
