09da7f174de1723f25f4013e7fc87f5fea1db215
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-06-15 19:34:47 -0300

    Requisito 4 - Lista categorias

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -2,18 +2,44 @@ import React from 'react';
 import * as api from '../services/api';
 
 class Category extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      categories: [],
+    };
+    this.handlerState = this.handlerState.bind(this);
+  }
+
+  componentDidMount() {
+    this.handlerState();
+  }
+
+  async handlerState() {
+    const response = await api.getCategories().then();
+    this.setState({
+      categories: response,
+    });
+  }
+
   render() {
-    const result = api.getCategories().then()
+    const { categories } = this.state;
     return (
       <div>
-        <ul>
-          {console.log(result)}
-          {/* {api.getCategories.map(({ name }) =>(<li>`${name}`</li>))} */}
-          {/* {console.log(api.getCategories().then())} */}
-        </ul>
+        <form>
+          {categories.map((category, index) => (
+            <label htmlFor="categories" key={ index }>
+              { category.name}
+              <input
+                key={ index }
+                data-testid="category"
+                type="radio"
+                name="categories"
+              />
+            </label>
+          ))}
+        </form>
       </div>
     );
   }
 }
-// {movies.map((movie) => <MovieCard key={ movie.title } movie={ movie } />)}
 export default Category;
