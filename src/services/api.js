7d8b14ac96f22895b3f3c63f461b47a98bff8b1e
PH Assis <pieit@live.com>
2021-06-15 17:16:54 -0300

    First commit

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,13 @@
+// import fetch from 'node-fetch';
+
 export async function getCategories() {
+  const URL = 'https://api.mercadolibre.com/sites/MLB/categories';
+  // const URL = 'https://randomuser.me/api/';
+  const response = await fetch(URL);
+  const resolved = await response.json();
+  console.log(resolved);
+  return resolved;
+
   // Implemente aqui
 }
 
