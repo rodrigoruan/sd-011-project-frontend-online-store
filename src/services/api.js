fe164dede321d0e7b6f9a34a17ae70e4928490ac
Jean Esteves <jean.esteves2001@gmail.com>
2021-06-15 16:05:01 -0300

    resolvendo lint

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 export async function getCategories() {
   const request = await fetch(
-    'https://api.mercadolibre.com/sites/MLB/categories'
+    'https://api.mercadolibre.com/sites/MLB/categories',
   );
   const response = await request.json();
   return response;
@@ -8,7 +8,7 @@ export async function getCategories() {
 
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
   const request = await fetch(
-    `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`
+    `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`,
   );
   const response = await request.json();
   return response;
