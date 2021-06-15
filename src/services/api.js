8169ea8231f2355a342d00e1df80fd610e7ca8b1
Julio Filizzola <juliofilizzola@hotmail.com>
2021-06-15 17:19:57 -0300

    [28]=segunda func

--- src/services/api.js
+++ src/services/api.js
@@ -4,6 +4,8 @@ export async function getCategories() {
   return categories;
 }
 
-export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
-  // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+export async function getProductsFromCategoryAndQuery( categoryId, query ) {
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
+  const categoriesQuery = await response.json();
+  return categoriesQuery;
 }
