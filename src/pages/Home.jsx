7c276ca27013e580523a7d92650cee96859776dc
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-06-15 20:56:35 -0300

    Requirement 5 done.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,46 +1,60 @@
 import React, { Component } from 'react';
-import * as Api from '../services/api';
-import ProductList from '../components/productlist';
-import CategoryList from '../components/CategoryList';
 import { Link } from 'react-router-dom';
+import * as Api from '../services/api';
+import ProductList from '../components/ProductList';
+import CategoryList from '../components/CategoryList';
 
 class Home extends Component {
   constructor() {
     super();
     this.state = {
-      Result: {},
-      search: " "
+      data: [],
+      search: '',
     };
 
     this.HandlerState = this.HandlerState.bind(this);
     this.RequestApi = this.RequestApi.bind(this);
   }
-  
+
   HandlerState(event) {
     const { target: { value } } = event;
-    this.setState({search: value})
+    this.setState({ search: value });
   }
-  
+
   RequestApi() {
     const { search } = this.state;
-    const products = Api.getProductsFromCategoryAndQuery("", search);
-    this.setState((state)=> ({
-      Result: products 
-     }))
+    Api.getProductsFromCategoryAndQuery('', search)
+      .then(({ results }) => {
+        this.setState({ data: results });
+      });
   }
 
   render() {
-   
+    const { data } = this.state;
     return (
       <div>
         <label htmlFor="search">
-          <input type="text" name="search" onChange={this.HandlerState}/>
+          <input
+            data-testid="query-input"
+            type="text"
+            name="search"
+            onChange={ this.HandlerState }
+          />
         </label>
-        <button type="submit" onClick={this.RequestApi}>Pesquisar</button>
+
+        <button
+          data-testid="query-button"
+          type="submit"
+          onClick={ this.RequestApi }
+        >
+          Pesquisar
+        </button>
         <h2 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </h2>
-        <ProductList />
+
+        <ProductList productsList={ data } />
+
         <Link to="/cart" data-testid="shopping-cart-button">Carrinho</Link>
         <CategoryList />
 
