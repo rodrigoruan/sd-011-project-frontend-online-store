7152ffd2c4696bd4ccdd5c56f83a817293c849d2
Joao_Lima <joaopaulo.gomeslima8@gmail.com>
2021-06-15 16:16:22 -0300

    Requirement 1 done.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
+import * as api from './services/api';
 
 function App() {
+  api.getProductsFromCategoryAndQuery('MLB1055', 'celular');
   return (
     <div>
       <p>Oi</p>
     </div>
-  )
+  );
 }
 
 export default App;
