0742edc1b26487e03930aa2f3162960291c5a97a
rodolfowolff <rodolfoluizwolff@gmail.com>
2021-06-15 20:15:47 -0300

    adiciona requisito 3

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { FaShoppingCart } from 'react-icons/fa';
 
 class Home extends React.Component {
   render() {
     return (
       <div>
-        <input type="text" />
-        <p
-          data-testid="home-initial-message"
-        >
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </p>
+        <header>
+          <Link to="/ShoppingCart" data-testid="shopping-cart-button">
+            <FaShoppingCart size={ 30 } />
+          </Link>
+          <input type="text" />
+          <p
+            data-testid="home-initial-message"
+          >
+            Digite algum termo de pesquisa ou escolha uma categoria.
+          </p>
+        </header>
       </div>
     );
   }
