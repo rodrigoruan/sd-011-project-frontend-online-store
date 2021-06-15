e49c36bbfec6770b94b42a1528616ba071c1e398
Nikolas <nikolas.silva1990@gmail.com>
2021-06-15 18:36:22 -0300

    Requisito 2 concluído

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,11 @@ import React, { Component } from 'react';
 class Home extends Component {
   render() {
     return (
-      <div> textInComponent </div>
+      <div>
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+      </div>
     );
   }
 }
