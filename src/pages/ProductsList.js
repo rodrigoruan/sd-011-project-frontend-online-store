bf8ad622015b1e900431e1797e217ca26da6123a
anaclarabck <ackyotoku@gmail.com>
2021-06-15 18:36:28 -0300

    Finalizado requisito 3 de novo

---
+++ src/pages/ProductsList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export default class ProductsList extends React.Component {
+  render() {
+    return (
+      <div>
+        <input type="text" />
+        <p data-testid="home-initial-message">
+          Digite algum termo de pesquisa ou escolha uma categoria.
+        </p>
+        <Link data-testid="shopping-cart-button" to="/shoppingcart">Carrinho</Link>
+      </div>
+    );
+  }
+}
