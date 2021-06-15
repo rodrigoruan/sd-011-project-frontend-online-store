0f2deba9dfb9e6a68eb1c3dfe7f54d6a3bf49df4
vitor-m-cardoso <vitor.mauricioc@outlook.com>
2021-06-15 18:52:26 -0300

    Liste os produtos buscados por termos, com os dados resumidos, associados a esses termos

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,28 +1,52 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Categories from './Categories';
+import SearchProduct from './SearchProduct';
 
 class Home extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
     this.state = {
       searchBar: '',
+      mostrarProdutos: false,
     };
   }
 
+  handleChange = ({ target }) => {
+    const { value, name } = target;
+
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  showProducts = () => {
+    this.setState({
+      mostrarProdutos: true,
+    });
+  }
+
   render() {
-    const { searchBar } = this.state;
+    const { searchBar, mostrarProdutos } = this.state;
     return (
       <div>
-        <label htmlFor="searchBar" data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
+        <label htmlFor="searchBar">
           <input
             type="text"
+            onChange={ this.handleChange }
             value={ searchBar }
             name="searchBar"
+            data-testid="query-input"
           />
         </label>
+        <button
+          type="button"
+          onClick={ () => this.showProducts() }
+          data-testid="query-button"
+        >
+          Pesquisar
+        </button>
         <Link
           to="/shoppingcart"
           data-testid="shopping-cart-button"
@@ -30,6 +54,11 @@ class Home extends Component {
           Ir para o carrinho
         </Link>
         <Categories />
+        <span data-testid="home-initial-message">
+          {mostrarProdutos
+            ? <SearchProduct searchBar={ searchBar } />
+            : <p>Digite algum termo de pesquisa ou escolha uma categoria.</p>}
+        </span>
       </div>
     );
   }
