26965edc7286a65b86eecf3b800be44181567caf
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2021-06-15 17:43:14 -0300

    Lista de categorias criada

---
+++ src/CategoryBar.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import * as api from './services/api';
+
+export default class CategoryBar extends Component {
+  constructor() {
+    super();
+    this.state = {
+      categoriesList: [],
+    };
+    this.getCategoryList = this.getCategoryList.bind(this);
+  }
+
+  componentDidMount() {
+    this.getCategoryList();
+  }
+
+  async getCategoryList() {
+    const categories = await api.getCategories();
+    const state = this.setState({
+      categoriesList: categories,
+    });
+    return state;
+  }
+
+  render() {
+    const { categoriesList } = this.state;
+    return (
+      <div className="categories-container">
+        <ul>
+          {categoriesList.map((category) => (
+            <li key={ category.id } data-testid="category">
+              {category.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+import CategoryBar from './CategoryBar';
 
 class Home extends Component {
   render() {
     return (
       <div>
+        <CategoryBar />
         <input />
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
