b2c2c50163eb42f6114c274c99e856df7e8c1452
Diogo Augusto <diogotrescastro@gmail.com>
2021-06-15 17:03:17 -0300

    Resoluçao do requisito 3

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+
+import ShopCart from './ShopCart';
 
 export default class Home extends Component {
   render() {
@@ -7,6 +10,14 @@ export default class Home extends Component {
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+        <Router>
+          <Link to="/cart" data-testid="shopping-cart-button">
+            <img src="./images/cart.svg" alt="Cart" />
+          </Link>
+          <Switch>
+            <Route path="/cart" component={ ShopCart } />
+          </Switch>
+        </Router>
       </div>
     );
   }
