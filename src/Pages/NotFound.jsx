e0e50b47997e6277b180ef7621855beeed298fd7
Nikolas <nikolas.silva1990@gmail.com>
2021-06-16 18:31:02 -0300

    Requisito 5 concluído

---
+++ src/Pages/NotFound.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NotFound extends Component {
+  render() {
+    return (
+      <div>
+        <p>Nenhum produto foi encontrado</p>
+      </div>
+    );
+  }
+}
+
+export default NotFound;
---
+++ src/Pages/Products.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import NotFound from './NotFound';
+
+class Products extends Component {
+  render() {
+    const { prodList } = this.props;
+    if (prodList.length === 0) {
+      return <NotFound />;
+    }
+    return (
+      <div>
+        { prodList.map((item) => (
+          <div key={ item.id } data-testid="product">
+            <h1>{item.title}</h1>
+            <img src={ item.thumbnail } alt={ item.title } />
+            <p>{`R$: ${item.price}`}</p>
+          </div>
+        ))}
+      </div>
+
+    );
+  }
+}
+
+Products.propTypes = {
+  prodList: PropTypes.arrayOf(
+    PropTypes.array,
+    PropTypes.object,
+  ).isRequired,
+};
+
+export default Products;
