feabd7d5b8ac6700bcc4edcd59d802e6530a336a
jhonatavbrg <jhonatavbrg@gmail.com>
2021-06-15 22:12:07 -0300

    Adicionado quinto requisito

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,15 +1,74 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { getProductsFromCategoryAndQuery } from '../services/api';
+import cart from '../images/cart.png';
+import Searchfield from './Searchfield';
+import Filteringbycategory from './Filteringbycategory';
 
-class Home extends React.Component {
+class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: '',
+      products: [],
+    };
+    this.onChange = this.onChange.bind(this);
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onChange({ target: { value } }) {
+    this.setState({
+      search: value,
+    });
+  }
+
+  async onClick() {
+    const { search } = this.state;
+    const products = await getProductsFromCategoryAndQuery('$categoryId', `$${search}`);
+    const { results } = products;
+    this.setState({
+      products: results,
+      search: '',
+    });
+  }
+
   render() {
+    const { products, search } = this.state;
     return (
       <div>
-        <input type="text" />
-        <p
-          data-testid="home-initial-message"
-        >
+        <form>
+          <label htmlFor="search-bar">
+            <input
+              type="text"
+              data-testid="query-input"
+              id="search-bar"
+              value={ search }
+              onChange={ this.onChange }
+            />
+          </label>
+          <button
+            data-testid="query-button"
+            onClick={ this.onClick }
+            type="button"
+          >
+            Buscar
+          </button>
+        </form>
+        <h1 data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
-        </p>
+        </h1>
+        <div>
+          <Link to="/shopping-cart">
+            <img
+              src={ cart }
+              alt="shopping-cart"
+              data-testid="shopping-cart-button"
+              height="80px"
+            />
+          </Link>
+        </div>
+        <Searchfield products={ products } />
+        <Filteringbycategory />
       </div>
     );
   }
