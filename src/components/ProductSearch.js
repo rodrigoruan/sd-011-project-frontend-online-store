3ad21ecdb00611da2d547d2cd2abb0e584b8dc11
Matheus Gaspar <msouzagaspar@gmail.com>
2021-06-16 11:08:19 -0300

    finaliza requisito 5

--- src/components/ProductSearch.js
+++ src/components/ProductSearch.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-class ProductSearch extends Component {
+export default class ProductSearch extends Component {
   render() {
+    const { handleSubmit, onChange, value } = this.props;
+
     return (
-      <>
+      <div>
         <input
+          data-testid="query-input"
           name="searchProduct"
           type="text"
+          value={ value }
+          onChange={ onChange }
         />
-        <span data-testid="home-initial-message">
-          Digite algum termo de pesquisa ou escolha uma categoria.
-        </span>
-      </>
+        <button
+          type="submit"
+          onClick={ handleSubmit }
+          data-testid="query-button"
+        >
+          Buscar
+        </button>
+      </div>
     );
   }
 }
-export default ProductSearch;
+
+ProductSearch.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
