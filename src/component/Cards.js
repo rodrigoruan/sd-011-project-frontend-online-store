b27cbe9d15212d1a12097e04920a9520fa280649
marlon307 <marlon-ramosb@hotmail.com>
2021-06-16 16:29:47 -0300

    requesito 7

--- src/component/Cards.js
+++ src/component/Cards.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 export default class Cards extends Component {
   render() {
     const { resultSearch } = this.props;
-
     return (
       <div className="cards">
         { resultSearch.map(({ id, title }) => (
           <div data-testid="product" key={ id }>
             <p>{ title }</p>
+            <Link
+              to={ `/product/${id}` }
+              data-testid="product-detail-link"
+            >
+              Ver detalhes
+            </Link>
           </div>
         )) }
       </div>
