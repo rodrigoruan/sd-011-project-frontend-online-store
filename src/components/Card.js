2192be6abe9455fb0f299a417ee8cc7422ff291b
Tulio Sploradori <tuliospl@gmail.com>
2021-06-16 19:19:49 -0300

    Requisito 8

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import Cart from './Cart';
 
 class Card extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      product: props.product,
+      loading: true,
+      cartList: [],
+    };
+    this.handlerSubmit = this.handlerSubmit.bind(this);
+  }
+
+  handlerSubmit() {
+    const { product, cartList } = this.state;
+    const { title, price, thumbnail } = product;
+    const cartProduct = (
+      <li>
+        <p>{ title }</p>
+        <img src={ thumbnail } alt={ title } />
+        <p>{ price }</p>
+      </li>
+    );
+    this.setState({
+      loading: false,
+      cartList: [...cartList, cartProduct],
+    });
+  }
+
   render() {
-    const { product } = this.props;
+    const { product, loading } = this.state;
     // console.log(product);
     const { title, thumbnail, price } = product;
     return (
@@ -19,6 +46,15 @@ class Card extends React.Component {
         >
           Detalhes
         </Link>
+        <br />
+        <button
+          type="submit"
+          data-testid="product-add-to-cart"
+          onClick={ this.handlerSubmit }
+        >
+          Adicionar ao Carrinho
+        </button>
+        {loading ? (<span>oi</span>) : (<Cart addCart={ product } />)}
       </div>
     );
   }
--- src/components/Cart.js
+++ src/components/Cart.js
@@ -3,10 +3,18 @@ import { Link } from 'react-router-dom';
 
 class Cart extends React.Component {
   render() {
+    const { addCart } = this.props;
+    const { title, price } = addCart;
+    console.log(addCart);
+    const mensagem = (<p data-testid="shopping-cart-empty-message">
+      Seu carrinho está vazio
+    </p>);
     return (
       <div>
         <Link to="/">Main</Link>
-        <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+        <ul>
+          
+        </ul>
       </div>
     );
   }
