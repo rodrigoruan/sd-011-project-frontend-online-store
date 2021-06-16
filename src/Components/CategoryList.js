ff6f5c9818797d18dcaf602dc9e0409fa7f020df
Nalu <aluziapantoja@gmail.com>
2021-06-16 17:11:35 -0300

    Requirement 4 - List available categories

---
+++ src/Components/CategoryList.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import * as api from '../services/api';
+
+export default class CategoryList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      categories: [],
+    };
+    this.getList = this.getList.bind(this);
+  }
+
+  componentDidMount() {
+    this.getList();
+  }
+
+  async getList() {
+    const listCategories = await api.getCategories()
+      .then((category) => this.setState({
+        categories: category,
+      }));
+    return listCategories;
+  }
+
+  render() {
+    const { categories } = this.state;
+    return (
+      <div>
+        <ul>
+          {categories.map((category) => (
+            <li
+              data-testid="category"
+              key={ category.id }
+            >
+              { category.name }
+            </li>))}
+        </ul>
+      </div>
+    );
+  }
+}
--- src/pages/ListProducts.js
+++ src/pages/ListProducts.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import Button from '../Components/ButtonShopCart';
+import CategoryList from '../Components/CategoryList';
 
 class ListProducts extends Component {
   render() {
     return (
       <div data-testid="home-initial-message">
+        <CategoryList />
         <input type="text" />
         <Button />
         Digite algum termo de pesquisa ou escolha uma categoria.
