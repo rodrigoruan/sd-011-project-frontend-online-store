118c2382641503d3ce25b1266ae695199c008d5f
Kevin Willian <kevinwillian084@gmail.com>
2021-06-16 16:48:14 -0300

    Refatorando requisito 05

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,9 @@ import { Link } from 'react-router-dom';
 import shoppingCartImage from '../images/shoppingCart.jpg';
 import * as Api from '../services/api';
 import CategoryList from '../components/categoryList';
+import SearchBarProducts from '../components/SearchBarProducts';
+import AllProducts from '../components/AllProducts';
+import Button from '../components/Button';
 
 class Home extends Component {
   constructor() {
@@ -11,11 +14,21 @@ class Home extends Component {
     this.state = {
       categories: [],
       loading: true,
+      products: [],
+      searchValue: '',
     };
+    this.handleSearch = this.handleSearch.bind(this);
   }
 
   componentDidMount() {
     this.fetchCategories();
+  }
+
+  handleSearch({ target }) {
+    const { value, name } = target;
+    this.setState({
+      [name]: value,
+    });
   }
 
   async fetchCategories() {
@@ -26,10 +39,21 @@ class Home extends Component {
     });
   }
 
+  async fetchProducts(query) {
+    const result = await Api.getProductsFromCategoryAndQuery('', query);
+    const products = result.results;
+    this.setState({
+      products,
+    });
+  }
+
   render() {
-    const { categories, loading } = this.state;
+    const { searchValue, products, categories, loading } = this.state;
     return (
       <div>
+        <SearchBarProducts value={ searchValue } onChange={ this.handleSearch } />
+        <Button onClick={ () => this.fetchProducts(searchValue) } innerText="Buscar" />
+        <AllProducts productsList={ products } />
         { !loading && categories
           .map((category, index) => <CategoryList key={ index } category={ category } />)}
         <span data-testid="home-initial-message">
