7b0362936f0fa39f4d46d7b3bfda0134d02035c8
Bruno Duarte <brunoarduarte@gmail.com>
2021-06-16 11:26:52 -0300

    Requisito 4

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Cart from './Components/Cart';
+import Categories from './Components/Categories';
 import Home from './Components/Home';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Switch>
           <Route exact path="/" component={ Home } />
           <Route path="/cart" component={ Cart } />
+          <Route path="/categories/:id" component={ Categories } />
         </Switch>
       </BrowserRouter>
     </div>
