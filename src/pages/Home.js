4ab98d8cf1ff95c1ce2d21c42dc4cfda5815ea4c
Carlos Henrique <henrique.joaovictor@hotmail.com>
2021-06-16 01:43:56 -0300

    Realizado requisito 3

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,23 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import imageTwo from '../imgs/Carrinho.png';
 import ProductSearch from '../components/ProductSearch';
 
 class Home extends Component {
   render() {
     return (
-      <ProductSearch />
+      <>
+        <ProductSearch />
+        <Link to="/ShoppingCart">
+          <img
+            width="30px"
+            src={ imageTwo }
+            alt="imagem do carrinho"
+          />
+        </Link>
+        <main data-testid="shopping-cart-button" />
+        <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+      </>
     );
   }
 }
