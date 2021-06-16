64e3191245ca789ea5800a87cd4b466609eab3f8
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-06-16 12:35:18 -0300

    Requisito 5 - Inicializa listagem de produtos.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,23 +1,56 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import CardItem from './CardItem';
 import Category from './Category';
-// import ShoppingCart from './ShoppingCart';
+import * as api from '../services/api';
 
 class Home extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      inputProduct: '',
+      response: [],
+    };
+
+    this.catchText = this.catchText.bind(this);
+    this.requestButton = this.requestButton.bind(this);
+  }
+
+  catchText({ target }) {
+    const { value } = target;
+    this.setState({
+      inputProduct: value,
+    });
+  }
+
+  async requestButton() {
+    const { inputProduct } = this.state;
+    const request = await api.getProductsFromCategoryAndQuery(undefined, inputProduct);
+    this.setState({
+      response: request.results,
+    });
+  }
+
   render() {
+    console.log('Render Home');
+    const { response } = this.state;
     return (
       <div className="main-page">
         <Category />
-        <div className="search-bar">
-          <form>
-            <label htmlFor="queryInput" data-testid="home-initial-message">
-              Digite algum termo de pesquisa ou escolha uma categoria.
-              <input id="queryInput" />
-            </label>
-          </form>
-          <Link to="/carrinho" data-testid="shopping-cart-button">
-            Carrinho de compras 🛒
-          </Link>
+        <div>
+          <div>
+            <form className="search-bar">
+              <label htmlFor="queryInput" data-testid="home-initial-message">
+                Digite algum termo de pesquisa ou escolha uma categoria.
+                <input data-testid="query-input" id="queryInput" onChange={this.catchText} />
+              </label>
+            </form>
+            <button type="button" onClick={ this.requestButton } data-testid="query-button"> Pesquisa </button>
+            <Link to="/carrinho" data-testid="shopping-cart-button">
+              Carrinho de compras 🛒
+            </Link>
+          </div>
+          <CardItem products={ response } />
         </div>
       </div>
     );
