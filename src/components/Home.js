d59e3f11a60a743ef68815022d7260bbf905b40c
Matheus Costa <mathws.costag@gmail.com>
2021-06-15 21:50:33 -0300

    muda o componente padrao de categoryId

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Filtros from './Filtros';
 import Input from './Input';
+import ListCards from './';
 
 export default class Home extends Component {
   constructor() {
