62c4851f883a784d839ff5c094554a4249288165
Oryange <oryangestrifezze@gmail.com>
2021-06-16 19:05:32 -0300

    Adiciona Requisito 10

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -5,6 +5,7 @@ export default class Cart extends Component {
     super();
     this.state = {
       products: [],
+      count: 0,
     };
   }
 
@@ -15,10 +16,31 @@ export default class Cart extends Component {
   getItemLocalStorage = () => {
     const getProducts = { ...localStorage };
     const arrayOfproducts = Object.values(getProducts).map((e) => JSON.parse(e));
-    this.setState({ products: arrayOfproducts });
+    this.setState({
+      products: arrayOfproducts,
+      count: arrayOfproducts.map(({ counter, title }) => ({ counter, title })),
+    });
   };
 
+  handleClick = ({ target: { id, name } }) => {
+    const product = JSON.parse(localStorage.getItem(id));
+    if (name === 'add') {
+      product.counter += 1;
+    }
+    if (name === 'sub' && product.counter > 1) {
+      product.counter -= 1;
+    }
+    localStorage.setItem(id, JSON.stringify(product));
+    this.getItemLocalStorage();
+  };
+
+  handleDelete = ({ target: { id } }) => {
+    localStorage.removeItem(id);
+    this.getItemLocalStorage();
+  }
+
   render() {
+    const { count } = this.state;
     const { products } = this.state;
 
     if (!products.length) {
@@ -28,23 +50,45 @@ export default class Cart extends Component {
     }
     return (
       <div>
-        {products.map(
-          ({ counter, title, price, thumbnail, id }) => (
-            <div key={ id }>
-              <p data-testid="shopping-cart-product-name">{title}</p>
-              <p
-                data-testid="shopping-cart-product-quantity"
-              >
-                { counter }
-              </p>
-              <img src={ thumbnail } alt={ title } />
-              <p>
-                R$
-                {price}
-              </p>
-            </div>
-          ),
-        )}
+        {products.map(({ title, price, thumbnail, id }) => (
+          <div key={ id }>
+            <p data-testid="shopping-cart-product-name">{title}</p>
+            <img src={ thumbnail } alt={ title } />
+            <p>
+              R$
+              {price}
+            </p>
+            <p data-testid="shopping-cart-product-quantity">
+              {count.find((item) => item.title === title).counter}
+            </p>
+            <button
+              data-testid="product-increase-quantity"
+              type="button"
+              name="add"
+              id={ title }
+              onClick={ this.handleClick }
+            >
+              (+)
+            </button>
+            <button
+              data-testid="product-decrease-quantity"
+              type="button"
+              name="sub"
+              id={ title }
+              onClick={ this.handleClick }
+            >
+              (-)
+            </button>
+            <button
+              type="button"
+              name="delete"
+              id={ title }
+              onClick={ this.handleDelete }
+            >
+              (X)
+            </button>
+          </div>
+        ))}
       </div>
     );
   }
