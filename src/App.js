82af1561a919526cae4d78a50c6819e3d67c72f5
Júlio Mota <juliomotacampos@gmail.com>
2021-06-16 20:48:23 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -2,13 +2,9 @@ import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import ShoppingCart from './Pages/ShoppingCart';
-// import * as api from './services/api';
-// test
 
 class App extends Component {
   render() {
-    // api.getCategories().then(categories => { console.log(categories) });
-    // api.getProductsFromCategoryAndQuery('MLB1055', 'Motorola').then(categories => { console.log(categories) });
 
     return (
       <BrowserRouter>
