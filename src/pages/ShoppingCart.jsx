06aaa97fc799d3fbe1c495e62583649130958c04
Anajulia Brito <juliehindius@gmail.com>
2021-06-16 09:50:43 -0300

    adicionado testid no shopping cart

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 class ShoppingCart extends Component {
   render() {
-    return <p>Seu carrinho está vazio</p>;
+    return <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>;
   }
 }
 
