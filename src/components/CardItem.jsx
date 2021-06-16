70be0f846b02f83a9b0a7591aa3870e98369c181
Vinicius Gouveia <freitas.viniciuspk@outlook.com.br>
2021-06-16 15:58:31 -0300

    Adiciona funcionalidade (Lista de itens)

--- src/components/CardItem.jsx
+++ src/components/CardItem.jsx
@@ -1,34 +1,9 @@
 import React from 'react';
 
 class CardItem extends React.Component {
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     products: [],
-  //   };
-
-  //   this.handlerState = this.handlerState.bind(this);
-
-  // }
-
-  // componentDidMount() {
-  //   this.handlerState();
-  // }
-
-  // async handlerState() {
-  //   // const queryTerm =
-  //   const response = await api.getProductsFromCategoryAndQuery().then();
-  //   this.setState({
-  //     products: response,
-  //   });
-  // }
 
   render() {
     const { products } = this.props;
-    // const { title } = products;
-    // const aguarde = (<p>Aguarde</p>);
-    // const bom = (<p>Bom</p>);
-
     return products.map(({ title, thumbnail, price }) => {
       return (
         <div data-testid="product">
@@ -38,7 +13,6 @@ class CardItem extends React.Component {
         </div>
       );
     });
-    // return products ? bom : ;
   }
 }
 export default CardItem;
