28500cb9f7407f73ac5470079978782ab7db5d4e
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-06-16 19:06:15 -0300

    requiisto 6

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -2,51 +2,19 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import ProductCard from './ProductCard';
-import * as api from '../services/api';
 
 export default class SearchResults extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      searchTerm: '',
-    };
-
-    this.handleSearch = this.handleSearch.bind(this);
-    this.handleChangeField = this.handleChangeField.bind(this);
-  }
-
-  componentDidMount() {
-    api.getCategories();
-  }
-
-  handleChangeField(e) {
-    this.setState({
-      searchTerm: e.target.value,
-    });
-  }
-
-  handleSearch(e) {
-    e.preventDefault();
-    const { updateSearchResults } = this.props;
-    const { searchTerm } = this.state;
-
-    api.getProductsFromCategoryAndQuery('', searchTerm)
-      .then((result) => updateSearchResults(result))
-      .catch((err) => console.error(err));
-  }
-
   render() {
-    const { searchResults } = this.props;
-    const { searchTerm } = this.state;
+    const { searchResults, handleSearch, handleChangeField, searchTerm } = this.props;
 
     return (
       <section id="home-search" className="home-two-fourths search-section">
         <header className="search-section__header">
-          <form onSubmit={ this.handleSearch }>
+          <form onSubmit={ handleSearch }>
             <input
               type="text"
               value={ searchTerm }
-              onChange={ this.handleChangeField }
+              onChange={ handleChangeField }
               data-testid="query-input"
             />
             <p
