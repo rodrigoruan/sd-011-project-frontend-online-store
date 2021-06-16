d87e1b0a66435130774823e2d4a6dddef2477919
Bruno Duarte <brunoarduarte@gmail.com>
2021-06-16 09:08:26 -0300

    Requisito 2 - Concluido

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
-import Main from './Main';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import './App.css';
+import Home from './Components/Home';
 
 function App() {
   return (
     <div className="App">
-      <Main />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Home } />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
