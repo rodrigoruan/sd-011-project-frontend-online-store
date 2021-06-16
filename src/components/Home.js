c4e7c207d84e1832fa09b3b9e3a8473ab15b4ffc
Arthur Hermann <arthurhermannc@gmail.com>
2021-06-15 23:00:33 -0300

    centraliza funcoes

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import ListCards from './ListCards';
 import Filtros from './Filtros';
+import { getProductsFromCategoryAndQuery } from '../services/api';
 import Input from './Input';
 
 export default class Home extends Component {
@@ -10,6 +11,7 @@ export default class Home extends Component {
     this.state = {
       query: '',
       category: 'MLB1384',
+      products: [],
     };
     this.handleOnChange = this.handleOnChange.bind(this);
   }
@@ -18,6 +20,18 @@ export default class Home extends Component {
     const { name } = target;
     const value = target.type === 'checkbox' ? target.checked : target.value;
     this.setState(({ [name]: value }));
+    this.searchApi();
+  }
+
+  async searchApi() {
+    const { query, category } = this.state;
+    console.log(query);
+    let products = await getProductsFromCategoryAndQuery(query, category);
+    console.log(products);
+    products = products.results.map(({ title, id, price }) => (
+      { name: title, key: id, price }
+    ));
+    this.setState(products);
   }
 
   render() {
