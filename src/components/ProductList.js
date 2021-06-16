b72677dcd41962e24e9908a36005ed992cd74ae2
Julio Filizzola <juliofilizzola@hotmail.com>
2021-06-16 11:59:32 -0300

    [28]= Proptypes referente ao arquivo ProductList

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export default class ProductList extends Component {
   render() {
@@ -7,8 +8,19 @@ export default class ProductList extends Component {
       <div data-testid="product">
         <p>{ title }</p>
         <img src={ thumbnail } alt={ title } />
-        <p>R$ {price}</p>
+        <p>
+          R$
+          {price}
+        </p>
       </div>
     );
   }
 }
+
+ProductList.propTypes = {
+  products: PropTypes.shape({
+    title: PropTypes.string,
+    price: PropTypes.number,
+    thumbnail: PropTypes.string,
+  }).isRequired,
+};
