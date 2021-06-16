acbdeee406d8872a56d27dbe53fe3a51a4679bb3
rubia braga <rubiaemilly29@gmail.com>
2021-06-16 15:06:03 -0300

    atualizacao dos componentes (criacao do componente SearchBar)

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Header from './page/Header';
+import MainPage from './page/MainPage';
 import ShoppingCart from './page/ShoppingCart';
 import CategorieNav from './page/CategorieNav';
 
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={ Header } />
+          <Route exact path="/" component={ MainPage } />
           <Route exact path="/shopping-cart" component={ ShoppingCart } />
           <Route exact path="/categorie-nav" component={ CategorieNav } />
         </Switch>
