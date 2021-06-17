bfcbee4f750e6cb3138b8f05a01bac39947f4d39
LeoFuna <leo.funa@gmail.com>
2021-06-16 21:31:53 -0300

    Ajustes para utilizaçao de Routers no APP.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+} from 'react-router-dom';
 import './App.css';
 import Home from './Pages/Home';
-import ShoppingCartButton from './Pages/ShoppingCartButton';
 import ShoppingCart from './Pages/ShoppingCart';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Home />
-        <ShoppingCartButton />
-        <ShoppingCart />
+        <Router>
+          <Switch>
+            <Route exact path="/">
+              <Home />
+              <Link data-testid="shopping-cart-button" to="/cart">Ir ao Carrinho</Link>
+            </Route>
+            <Route path="/cart">
+              <ShoppingCart />
+              <Link to="/">Voltar à Home</Link>
+            </Route>
+          </Switch>
+        </Router>
       </div>);
   }
 }
