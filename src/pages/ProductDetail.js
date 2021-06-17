56fd6962c3ee18282ea8f1343ae4de9d81ff7fc8
renan <renan_menezes07@hotmail.com>
2021-06-17 16:00:07 -0300

    Ajustes no requisito 11.

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,40 +1,37 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-<<<<<<< HEAD
 // import CustomerRatingForm from '../components/CustomerRatingForm';
-=======
 import CartButton from '../components/CartButton';
->>>>>>> c23f619ca5f457f7ed9769459bcad4d15e8326f8
 
 class ProductDetail extends Component {
-  constructor() {
-    super();
+  // constructor() {
+  //   super();
 
-    this.state = {
-      email: '',
-      comment: '',
-      rating: 1,
-      counter: 0,
-    };
+  //   this.state = {
+  //     email: '',
+  //     comment: '',
+  //     rating: 1,
+  //     counter: 0,
+  //   };
 
-    this.handleChange = this.handleChange.bind(this);
-    this.submitRating = this.submitRating.bind(this);
-  }
+  //   this.handleChange = this.handleChange.bind(this);
+  //   this.submitRating = this.submitRating.bind(this);
+  // }
 
-  handleChange({ target: { value, name } }) {
-    this.setState({
-      [name]: value,
-    });
-  }
+  // handleChange({ target: { value, name } }) {
+  //   this.setState({
+  //     [name]: value,
+  //   });
+  // }
 
-  async submitRating() {
-    await this.setState((state) => ({
-      counter: state.counter + 1,
-    }));
-    const { counter } = this.state;
-    // const commentList = [{ email, comment, rating, counter }];
-    localStorage.setItem(counter, JSON.stringify(this.state));
-  }
+  // async submitRating() {
+  //   await this.setState((state) => ({
+  //     counter: state.counter + 1,
+  //   }));
+  //   const { counter } = this.state;
+  //   // const commentList = [{ email, comment, rating, counter }];
+  //   localStorage.setItem(counter, JSON.stringify(this.state));
+  // }
 
   render() {
     const {
@@ -42,9 +39,9 @@ class ProductDetail extends Component {
       // { state:
       //   { produto:
       //     { condition, price, thumbnail, title } } } } = this.props;
-    // const { email, comment, rating, counter } = this.state;
-    // const storedRatings = JSON.parse(localStorage.getItem(counter));
-    // console.log(storedRatings);
+      // const { email, comment, rating, counter } = this.state;
+      // const storedRatings = JSON.parse(localStorage.getItem(counter));
+      // console.log(storedRatings);
         { state:
           { produto, addToCart } } } = this.props;
     const { condition, price, thumbnail, title } = produto;
@@ -108,8 +105,8 @@ class ProductDetail extends Component {
             state: { cart: addToCart },
           } }
         />
-          <input type="text" />
-          <textarea type="text" data-testid="product-detail-evaluation" />
+        <input type="text" />
+        <textarea type="text" data-testid="product-detail-evaluation" />
       </div>
     );
   }
