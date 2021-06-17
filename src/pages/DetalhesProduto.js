fa449b40958263b9127f51d34b90c7c4b9bb28eb
Thiago Gomes <thiagog.macedom@gmail.com>
2021-06-16 21:09:33 -0300

    requisito 7 corrigindo problemas

--- src/pages/DetalhesProduto.js
+++ src/pages/DetalhesProduto.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { getProductsFromCategoryAndQuery } from './services/api';
-import Loading from './Loading';
+import { getProductsFromCategoryAndQuery } from '../services/api';
+import Loading from '../components/Loading';
 
 class DetalhesProduto extends Component {
   constructor() {
