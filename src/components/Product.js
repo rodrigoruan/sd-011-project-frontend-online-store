44bc410c5c908b5f3dfca50f9d3b3791fff9df64
Rodrigo Ruan <rodrigopython16@gmail.com>
2021-06-16 22:41:55 -0300

    Adiciona requisito 11

--- src/components/Product.js
+++ src/components/Product.js
@@ -7,7 +7,35 @@ export default class Product extends Component {
     super();
     this.state = {
       counter: 1,
+      comment: '',
+      stars: '',
+      allComments: null,
     };
+  }
+
+  componentDidMount() {
+    this.saveCommentOnLocal();
+  }
+
+  setComments = ({ target: { value, name } }) => {
+    this.setState({ [name]: value });
+  };
+
+  saveCommentOnLocal = () => {
+    const { location: { state: { id } } } = this.props;
+    const { comment, stars } = this.state;
+
+    if (stars) {
+      const object = { comment, stars };
+      const prevStorage = localStorage.getItem(id);
+      if (prevStorage) {
+        localStorage.setItem(id, `${prevStorage}*${JSON.stringify(object)}`);
+      }
+      if (prevStorage === null && stars !== 'Avaliar') {
+        localStorage.setItem(id, JSON.stringify(object));
+      }
+    }
+    this.setState({ allComments: localStorage.getItem(id) });
   }
 
   handleClick = () => {
@@ -15,15 +43,23 @@ export default class Product extends Component {
       counter: previous.counter + 1,
     }));
     const { counter } = this.state;
-    const { location: { state } } = this.props;
+    const {
+      location: { state },
+    } = this.props;
     const { title, id, price, thumbnail, attributes } = state;
     const object = { counter, price, thumbnail, id, attributes, title };
     const json = JSON.stringify(object);
     localStorage.setItem(title, json);
-  }
+  };
 
   render() {
-    const { location: { state: { title, price, thumbnail, attributes } } } = this.props;
+    const {
+      location: {
+        state: { title, price, thumbnail, attributes, id },
+      },
+    } = this.props;
+
+    const { allComments } = this.state;
     return (
       <>
         <h1 data-testid="product-detail-name">{title}</h1>
@@ -34,11 +70,8 @@ export default class Product extends Component {
         </p>
         <div>
           <ul>
-            {attributes && attributes.map(({ name }, index) => (
-              <li key={ index }>
-                {name}
-              </li>
-            ))}
+            {attributes
+              && attributes.map(({ name }, index) => <li key={ index }>{name}</li>)}
           </ul>
         </div>
         <button
@@ -48,7 +81,45 @@ export default class Product extends Component {
         >
           Adicionar ao Carrinho
         </button>
-        <Link data-testid="shopping-cart-button" to="/cart">Carrinho</Link>
+        <Link data-testid="shopping-cart-button" to="/cart">
+          Carrinho
+        </Link>
+        <div>
+          <textarea
+            name="comment"
+            onChange={ this.setComments }
+            data-testid="product-detail-evaluation"
+            type="text"
+          />
+          <select name="stars" onChange={ this.setComments }>
+            <option>Avaliar</option>
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+          </select>
+          <button name={ id } type="button" onClick={ this.saveCommentOnLocal }>
+            Enviar comentário
+          </button>
+        </div>
+        <div>
+          <h4>Avaliações</h4>
+          {allComments && allComments.split('*').map((item, index) => {
+            const { comment, stars } = JSON.parse(item);
+            return (
+              <div key={ index }>
+                <p>
+                  {comment}
+                </p>
+                <p>
+                  STARS:
+                  {stars}
+                </p>
+              </div>
+            );
+          })}
+        </div>
       </>
     );
   }
