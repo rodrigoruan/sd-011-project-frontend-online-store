02a79c6cf37cd047ff2ad85ff4ee3ead75221743
Laura Gusmao <laurargus@gmail.com>
2021-06-16 21:57:36 -0300

    Adiciona requisito 7

--- src/components/Cardproduct.js
+++ src/components/Cardproduct.js
@@ -1,14 +1,25 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class Cardproduct extends Component {
   render() {
-    const { title, img, price } = this.props;
+    const { title, img, price, id, categoryId } = this.props;
     return (
       <div data-testid="product">
         <p>{ title }</p>
         <img src={ img } width="100px" alt="produto" />
-        <p>{ price }</p>
+        <p>
+          R$
+          {' '}
+          { price }
+        </p>
+        <Link
+          to={ `/product-detail/${categoryId}/${id}` }
+          data-testid="product-detail-link"
+        >
+          Ver Detalhes
+        </Link>
       </div>
     );
   }
@@ -18,6 +29,8 @@ Cardproduct.propTypes = {
   title: PropTypes.string.isRequired,
   img: PropTypes.string.isRequired,
   price: PropTypes.number.isRequired,
+  id: PropTypes.string.isRequired,
+  categoryId: PropTypes.string.isRequired,
 };
 
 export default Cardproduct;
---
+++ src/components/ProductDetails.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaShoppingCart } from 'react-icons/fa';
+import PropTypes from 'prop-types';
+import * as api from '../services/api';
+
+class ProductDetails extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      product: [],
+    };
+
+    this.getProduct = this.getProduct.bind(this);
+  }
+
+  componentDidMount() {
+    this.getProduct();
+  }
+
+  async getProduct() {
+    const { match: { params: { categoryId, id } } } = this.props;
+    const productObj = await api.getProductsFromCategoryAndQuery(categoryId, '');
+    const details = productObj.results
+      .find((product) => product.id === id);
+    this.setState({ product: details });
+  }
+
+  render() {
+    const { product: { title, thumbnail, price } } = this.state;
+    return (
+      <div>
+        <Link to="/">
+          Página Inicial
+        </Link>
+        <h3 data-testid="product-detail-name">{ title }</h3>
+        <div>
+          <img src={ thumbnail } alt="product" />
+          <p>
+            Preço: R$
+            {' '}
+            { price }
+          </p>
+        </div>
+        <Link to="/ShoppingCart" data-testid="shopping-cart-button">
+          <FaShoppingCart size={ 30 } />
+        </Link>
+      </div>
+    );
+  }
+}
+
+ProductDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+      categoryId: PropTypes.string,
+    }).isRequired,
+  }).isRequired,
+};
+
+export default ProductDetails;
