4c68aa9331fcfecd69f375d3c93d156bced728ec
rubia braga <rubiaemilly29@gmail.com>
2021-06-17 15:02:28 -0300

    req5

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+export default class ProductCard extends Component {
+  render() {
+    const { product: { title, price, thumbnail } } = this.props;
+    return (
+      <div data-testid="product">
+        <h5>{title}</h5>
+        <p>{price}</p>
+        <div>
+          <img src={ thumbnail } alt={ title } width="70px" />
+        </div>
+      </div>
+    );
+  }
+}
+
+ProductCard.propTypes = {
+  product: PropTypes.shape({
+    title: PropTypes.string,
+    price: PropTypes.number,
+    thumbnail: PropTypes.string,
+  }).isRequired,
+};
---
+++ src/components/SearchCategorie.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class SearchCategorie extends Component {
+  const{}=this.props
+  render() {
+    return (
+      <div>
+        
+      </div>
+    )
+  }
+}
