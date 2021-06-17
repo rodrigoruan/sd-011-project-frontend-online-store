8b380cc0a3f2f71cf0cacc2fb0bd4df292d864d3
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-06-17 16:33:25 -0300

    Requisito 8 Realizado

---
+++ src/components/AddToCart.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class AddToCart extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.addItem = this.addItem.bind(this);
+  }
+
+  addItem() {
+    const { item } = this.props;
+    let existingCart = JSON.parse(localStorage.getItem('items'));
+    if (existingCart == null) existingCart = [];
+    existingCart.push(item);
+    localStorage.setItem('items', JSON.stringify(existingCart));
+  }
+
+  readLocalStorage() {
+    console.log(JSON.parse(localStorage.getItem('items')));
+  }
+
+  render() {
+    return (
+      <div>
+        <button
+          type="button"
+          data-testid="product-add-to-cart"
+          onClick={ () => this.addItem() }
+        >
+          Adicionar ao Carrinho
+        </button>
+        <button
+          type="button"
+          data-testid="product-add-to-cart"
+          onClick={ () => this.readLocalStorage() }
+        >
+          teste
+        </button>
+      </div>
+    );
+  }
+}
+
+export default AddToCart;
+
+AddToCart.propTypes = {
+  item: PropTypes.objectOf(PropTypes.object()).isRequired,
+};
