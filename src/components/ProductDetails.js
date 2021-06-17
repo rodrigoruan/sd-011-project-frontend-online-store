8aede9ab9b61da18283d9797b9af25b4aa7fb78f
Rodolfo <rodolforrcont@gmail.com>
2021-06-16 21:54:06 -0300

    Adicione um produto ao carrinho a partir de sua tela de exibição detalhada

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -3,12 +3,50 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
 export default class ProductDetails extends Component {
+  constructor() {
+    super();
+
+    this.handleSetStorage = this.handleSetStorage.bind(this);
+  }
+
+  handleSetStorage(param) {
+    console.log(param);
+    param.countProduct = 1; // Adicionando um chave para contar
+    if (!localStorage.item) {
+      localStorage.setItem('item', JSON.stringify([param])); // transformando o objeto em JSON
+    } else {
+      const arrayObject = JSON.parse(localStorage.getItem('item')); // Retorna o JSON para objeto
+      const idProducts = arrayObject.map((value) => value.id);
+      const num = idProducts.indexOf(param.id); // Se não existe retorna -1, se existe o index do array é retornado.
+      if (num < 0) {
+        localStorage.setItem('item', JSON.stringify([...arrayObject, param])); // Adicionando objeto não existente no localStorage
+      } else {
+        arrayObject[num].countProduct += 1;
+        localStorage.setItem('item', JSON.stringify([...arrayObject])); // Se o id identificado já existir, mantem o localStorage
+      }
+    }
+  }
+
   render() {
-    const { location: { state: { result: { title, thumbnail, price } } } } = this.props;
+    const { location: { state: { result } } } = this.props;
+    const { title, thumbnail, price } = result;
     return (
       <div>
         <h2 data-testid="product-detail-name">{`${title} - ${price}`}</h2>
         <img src={ thumbnail } alt={ title } />
+        <button
+          data-testid="product-detail-add-to-cart"
+          type="button"
+          onClick={ () => this.handleSetStorage(result) }
+        >
+          Comprar
+        </button>
+        <Link
+          data-testid="shopping-cart-button"
+          to={ { pathname: '/ShoppingCart' } }
+        >
+          <button type="button">Carrinho</button>
+        </Link>
         <Link to="/">Voltar</Link>
       </div>
     );
--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -11,10 +11,11 @@ export default class ShoppingCart extends Component {
             Seu carrinho está vazio
           </div>
         ) : (
-          arrayObject.map(({ title, thumbnail, price }, index) => (
+          arrayObject.map(({ title, thumbnail, price, countProduct }, index) => (
             <div key={ index } data-testid="shopping-cart-product-name">
               <h2>{`${title}-${price}`}</h2>
               <img src={ thumbnail } alt={ title } />
+              <div data-testid="shopping-cart-product-quantity">{countProduct}</div>
             </div>
           ))
         )}
