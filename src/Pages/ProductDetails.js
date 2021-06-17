f61597554924032c9b018a62be695052ddf2aefc
Gabriel <gabrielfernandessilva.ba@gmail.com>
2021-06-17 08:01:35 -0300

    Requisito 9 Feito

--- src/Pages/ProductDetails.js
+++ src/Pages/ProductDetails.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Redirect } from 'react-router-dom';
+import { Redirect, Link } from 'react-router-dom';
 
 class ProductDetails extends Component {
   constructor(props) {
@@ -7,16 +7,17 @@ class ProductDetails extends Component {
     this.state = {
       redirect: false,
       product: {},
+      cartProducts: [],
     };
 
     this.setProduct = this.setProduct.bind(this);
     this.setRedirect = this.setRedirect.bind(this);
+    this.addCart = this.addCart.bind(this);
   }
 
   componentDidMount() {
     const product = JSON.parse(sessionStorage.getItem('product'));
     if (!product) this.setRedirect();
-    console.log(product);
     this.setProduct(product);
   }
 
@@ -32,14 +33,34 @@ class ProductDetails extends Component {
     }));
   }
 
+  addCart(product) {
+    this.setState((old) => ({
+      cartProducts: [...old.cartProducts, product],
+    }));
+  }
+
   render() {
-    const { redirect, product } = this.state;
+    const { redirect, product, cartProducts } = this.state;
     const { title, thumbnail, price } = product;
     return !redirect ? (
       <div>
         <p data-testid="product-detail-name">{ title }</p>
         <img src={ thumbnail } alt={ title } />
         { price }
+        <button
+          onClick={ () => this.addCart(product) }
+          type="button"
+          data-testid="product-detail-add-to-cart"
+        >
+          Adicionar ao carrinho
+        </button>
+        <Link
+          to={ { pathname: '/shopcart', state: cartProducts } }
+        >
+          <button type="button" data-testid="shopping-cart-button">
+            Carrinho de Compras
+          </button>
+        </Link>
       </div>
     ) : (<Redirect to="/" />);
   }
