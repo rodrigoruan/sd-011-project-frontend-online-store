997fe20553449181fc8968790fe66b8bcb99fbe3
¨Hugosomers¨ <hugosomersla@gmail.com>
2021-06-17 14:43:14 -0300

    Estilização cart

--- src/components/ShoppingCart/index.jsx
+++ src/components/ShoppingCart/index.jsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import './ShoppingCart.css';
 
 export default class ShoppingCart extends Component {
   render() {
     return (
       <div>
-        <Link to="/">Voltar</Link>
+        <header className="cartHeader">
+          <Link to="/" className="LinkBack">Voltar</Link>
+        </header>
         <h1 data-testid="shopping-cart-empty-message">Seu carrinho está vazio</h1>
       </div>
     );
