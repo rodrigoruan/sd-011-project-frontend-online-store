7a098dc3fbcfd1184cec1d15435feab134e7d43c
Marco Galindo <marco1408@gmail.com>
2021-06-17 17:01:47 -0300

    requisito 9 pendente

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -4,21 +4,18 @@ import PropTypes from 'prop-types';
 class Details extends Component {
   constructor(props) {
     super(props);
-    const { match } = this.props;
     const { location } = this.props;
     this.state = {
-      productId: match.params.id,
       productDetails: location.aboutProps,
     };
   }
 
   render() {
-    const { productDetails, productId } = this.state;
+    const { productDetails } = this.state;
     const { preco, image, name } = productDetails;
     return (
       <div>
         <img src={ image } alt={ name } />
-        <p>{ productId }</p>
         <p data-testid="product-detail-name">{name}</p>
         <p>{`R$: ${preco}`}</p>
       </div>
