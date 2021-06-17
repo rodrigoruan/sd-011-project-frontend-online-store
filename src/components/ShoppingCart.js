b166988e256a17ca90ea59089bc1403250b54028
guinhoanjos <iago.anjos@hotmail.com>
2021-06-17 18:28:30 -0300

    requisito 10 feito, eu acho

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,45 +1,86 @@
 import React, { Component } from 'react';
-import { results } from '../__mocks__/query';
 import closeButton from '../images/close-button.png';
 
 export default class ShoppingCart extends Component {
   constructor() {
     super();
-    localStorage.setItem('items', JSON.stringify(results));
+    this.increaseQuantity = this.increaseQuantity.bind(this);
+    this.decreaseQuantity = this.decreaseQuantity.bind(this);
+    this.removeAllItems = this.removeAllItems.bind(this);
+
     const items = JSON.parse(localStorage.getItem('items'));
     this.state = {
       items,
     };
   }
 
+  increaseQuantity(event) {
+    const { items } = this.state;
+    const { target: { id } } = event;
+
+    items.forEach((product, index) => {
+      if (product.id === id) {
+        items[index].quantity += 1;
+      }
+    });
+    this.setState({
+      items,
+    });
+  }
+
+  decreaseQuantity(event) {
+    const { items } = this.state;
+    const { target: { id } } = event;
+
+    items.forEach((product, index) => {
+      if (product.id === id && product.quantity >= 1) {
+        items[index].quantity -= 1;
+      }
+    });
+    this.setState({
+      items,
+    });
+  }
+
+  removeAllItems(event) {
+    const { items } = this.state;
+    const { target: { id } } = event;
+    this.setState({
+      items: items.filter((product) => product.id !== id),
+    });
+  }
+
   render() {
     const { items } = this.state;
     return (
       <div>
-        { items.map(({ title, thumbnail, price, id }, index) => (
+        { items.map(({ title, thumbnail, price, id, quantity }, index) => (
           <div key={ index }>
-            <img src={ closeButton } alt="close button" />
+            <img
+              src={ closeButton }
+              alt="close button"
+              id={ id }
+              onClick={ this.removeAllItems }
+            />
             <img src={ thumbnail } alt="Foto do Produto" />
             <p>{title}</p>
             <button
               id={ id }
               type="button"
               data-testid="product-decrease-quantity"
-              // onClick={ decreaseQuantity }
+              onClick={ this.decreaseQuantity }
             >
               -
             </button>
             <p>
               Quantidade:
-              {
-                items.reduce((acc, item) => (item.id === id ? acc + 1 : acc), 0)
-              }
+              { quantity }
             </p>
             <button
               id={ id }
               type="button"
               data-testid="product-increase-quantity"
-              // onClick={ increaseQuantity }
+              onClick={ this.increaseQuantity }
             >
               +
             </button>
@@ -47,7 +88,9 @@ export default class ShoppingCart extends Component {
             <p>{ `Valor total: R$${price}` }</p>
           </div>
         )) }
-        <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+        {items.length === 0
+          ? <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
+          : <p />}
       </div>
     );
   }
