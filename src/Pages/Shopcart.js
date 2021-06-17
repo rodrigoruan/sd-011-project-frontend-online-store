d333addae813ca0c338fc3bf407e46ae1c3b1d88
Gabriel <gabrielfernandessilva.ba@gmail.com>
2021-06-17 08:47:15 -0300

    Corrigindo um Bug que apareceu depois do ultimo push

--- src/Pages/Shopcart.js
+++ src/Pages/Shopcart.js
@@ -4,42 +4,47 @@ import PropTypes from 'prop-types';
 class Shopcart extends Component {
   constructor(props) {
     super(props);
-
-    // const { params } = this.props;
     const { location: { state } } = this.props;
     this.state = {
-      state,
-      empty: false,
+      products: state,
+      loading: true,
     };
 
     this.setLoading = this.setLoading.bind(this);
   }
 
   componentDidMount() {
-    const { state } = this.state;
-    if (state) this.setLoading();
+    this.setLoading();
   }
 
   setLoading() {
-    this.setState(() => ({
-      empty: true,
-    }));
+    const { products } = this.state;
+    if (products.length === 0) {
+      this.setState({
+        loading: true,
+      });
+    } else {
+      this.setState({
+        loading: false,
+      });
+    }
   }
 
   render() {
-    const { state, empty } = this.state;
-    return !empty ? (
-      <div data-testid="shopping-cart-empty-message">
-        Seu carrinho está vazio
-      </div>
-    ) : (
-      state.map((product, index) => (
-        <div key={ index }>
-          <h4 data-testid="shopping-cart-product-name">{product.title}</h4>
-          <h5 data-testid="shopping-cart-product-quantity">11</h5>
+    const { products, loading } = this.state;
+    return (
+      loading ? (
+        <div data-testid="shopping-cart-empty-message">
+          Seu carrinho está vazio
         </div>
-      ))
-    );
+      ) : (
+        products.map((product, index) => (
+          <div key={ index }>
+            <h4 data-testid="shopping-cart-product-name">{product.title}</h4>
+            <h5 data-testid="shopping-cart-product-quantity">11</h5>
+          </div>
+        ))
+      ));
   }
 }
 
