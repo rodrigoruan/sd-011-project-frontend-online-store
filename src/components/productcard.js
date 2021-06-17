4d3ab5dcaf832d7182ac14cdb127d0c5baa6546b
IgorMendez <igor.emp@hotmail.com>
2021-06-16 23:30:41 -0300

    Exercício 8[1]

--- src/components/productcard.js
+++ src/components/productcard.js
@@ -4,21 +4,22 @@ import PropTypes from 'prop-types';
 export default class ProductCard extends Component {
   constructor() {
     super();
-    this.state ={
+    this.state = {
       quantity: 1,
-    }
+    };
     this.handleAddCart = this.handleAddCart.bind(this);
   }
 
   handleAddCart() {
     const { quantity } = this.state;
     const { id, title, thumbnail, price } = this.props;
-    window.localStorage.setItem(id, [title, '/n', thumbnail, '/n', price, '/n', quantity]);
+    window.localStorage
+      .setItem(id, [title, '/n', thumbnail, '/n', price, '/n', quantity]);
     console.log(this.props);
   }
 
   render() {
-    const { title, thumbnail, price, id } = this.props;
+    const { title, thumbnail, price } = this.props;
     return (
       <div data-testid="product">
         <img src={ thumbnail } alt={ title } />
