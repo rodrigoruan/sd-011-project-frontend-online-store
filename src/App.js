435a469089c1efac9d2e990020164eb9530f16da
RobsonCardozo <robsonleaocardozo@hotmail.com>
2021-06-17 16:29:13 -0300

    Requisito 8 1.0

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React, { Component } from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import ShoppingCart from './Pages/ShoppingCart';
+import './App.css';
 // import * as api from './services/api';
-// test
 
 class App extends Component {
   render() {
@@ -13,7 +13,11 @@ class App extends Component {
     return (
       <BrowserRouter>
         <Switch>
-          <Route exact path="/shoppingcart" render={ () => <ShoppingCart /> } />
+          <Route
+            exact
+            path="/shoppingcart"
+            render={ () => (<ShoppingCart { ...props } />) }
+          />
           <Route exact path="/" render={ () => <Home /> } />
         </Switch>
       </BrowserRouter>
--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -3,22 +3,48 @@ import PropTypes from 'prop-types';
 import NotFound from './NotFound';
 
 class Products extends Component {
+  constructor(props) {
+    super(props);
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  // implementar variável "prodList"
+  handleClick() {
+    const { prodList } = this.props;
+    const cardList = this.loadList();
+
+    productList.push({ prodList });
+    localStorage.setItem('cart', JSON.stringify(cardList));
+  }
+
+  loadList() {
+    const cart = localStorage.getItem('cart');
+    return JSON.parse(cart);
+  }
+
   render() {
     const { prodList } = this.props;
-    if (prodList.length === 0) {
-      return <NotFound />;
-    }
+    if (prodList.length === 0) return <NotFound />;
+
     return (
       <div>
         { prodList.map((item) => (
           <div key={ item.id } data-testid="product">
-            <h1>{item.title}</h1>
+            <h1 data-testid="shopping-cart-product-name">{item.title}</h1>
             <img src={ item.thumbnail } alt={ item.title } />
+            <p data-testid="shopping-cart-product-quantity">{item.quantity}</p>
             <p>{`R$: ${item.price}`}</p>
+            <button
+              type="button"
+              data-testid="product-add-to-cart"
+              onClick={ this.handleClick }
+            >
+              Adicionar ao Carrinho
+            </button>
           </div>
         ))}
       </div>
-
     );
   }
 }
