4210fbd25376410b268a98ddca29ba68bc2b0423
renan <renan_menezes07@hotmail.com>
2021-06-17 07:10:55 -0300

    algumas alterações no requisito 11.

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -1,20 +1,47 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import CustomerRatingForm from '../components/CustomerRatingForm';
 
 class ProductDetail extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      email: '',
+      comment: '',
+      rating: 1,
+      commentList: [email, comment, rating],
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.submitRating = this.submitRating.bind(this);
+    this.setCustomerComment = this.setCustomerComment.bind(this);
+  }
+
+  handleChange({ target: { value, name } }) {
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  submitRating() {
+    this.setState((state) => ({
+      commentList: [state.commentList, [email, comment, rating]] }));
+  }
+
   render() {
-    console.log(this);
     const {
       location:
-        { state:
-          { produto:
-            { condition, price, thumbnail, title } } } } = this.props;
+      { state:
+        { produto:
+          { condition, price, thumbnail, title } } } } = this.props;
     return (
       <div>
         <h1 data-testid="product-detail-name">{ title }</h1>
         <h2>{ price }</h2>
         <img src={ thumbnail } alt={ `imagem de ${title}` } />
         <p>{ condition }</p>
+        <CustomerRatingForm info={ this.state } />
       </div>
     );
   }
