9f72a7284b3cad38348127a0c5f465ce9c52171a
Michelle <mls.srbek@gmail.com>
2021-06-17 17:12:31 -0300

    req 7 - ProductDetails page

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 class ProductCard extends React.Component {
   render() {
-    const { item: { title, thumbnail, price } } = this.props;
+    const { item: { title, thumbnail, price, id } } = this.props;
     return (
       <div data-testid="product" className="product-card">
         <p>{title}</p>
         <img src={ thumbnail } alt="imagem produto" />
         <p>{price}</p>
+        <Link data-testid="product-detail-link" to={ `/product-details/${id}` }>
+          DETALHES
+        </Link>
       </div>
     );
   }
@@ -18,7 +22,8 @@ ProductCard.propTypes = {
   item: PropTypes.shape({
     title: PropTypes.string,
     thumbnail: PropTypes.string,
-    price: PropTypes.string,
+    price: PropTypes.number,
+    id: PropTypes.string,
   }).isRequired,
 };
 
---
+++ src/pages/ProductDetails.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class ProductDetails extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      product: {},
+    };
+
+    this.fetchProduct = this.fetchProduct.bind(this);
+  }
+
+  componentDidMount() {
+    this.fetchProduct();
+  }
+
+  async fetchProduct() {
+    const { match: { params: { id } } } = this.props;
+    const response = await fetch(`https://api.mercadolibre.com/items/${id}?include_attributes=all`);
+    const responseJSON = await response.json();
+    this.setState({ product: responseJSON });
+  }
+
+  mapProd() {
+
+  }
+
+  render() {
+    const { product: { title, price, thumbnail, warranty, condition } } = this.state;
+    return (
+      <section>
+        <p data-testid="product-detail-name">{ title }</p>
+        <p>{ price }</p>
+        <img src={ thumbnail } alt="product" />
+        <div>
+          <p>{ warranty }</p>
+          <p>{ condition }</p>
+        </div>
+      </section>
+    );
+  }
+}
+
+ProductDetails.propTypes = {
+  match: PropTypes.shape({
+    params: PropTypes.shape({
+      id: PropTypes.string,
+    }),
+  }),
+}.isRequired;
+
+export default ProductDetails;
