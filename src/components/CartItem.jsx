d9245f891f1b5d3eff69ca1eca1bfb97551d5ae3
Jean Esteves <jean.esteves2001@gmail.com>
2021-06-17 08:31:13 -0300

    implementa botoes para aumentar e diminuir a quantidade de cada item no cart

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -4,33 +4,60 @@ import PropTypes from 'prop-types';
 export default class CartItem extends Component {
   constructor(props) {
     super(props);
-    const { cart: { price } } = this.props;
     this.state = {
       quantity: 1,
-      totalPrice: price,
     };
-    // this.handeClick = this.handeClick.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(operator) {
+    const { quantity } = this.state;
+    if (operator === 'increase') {
+      this.setState((previousState) => ({
+        quantity: previousState.quantity + 1,
+      }));
+    }
+    if (operator === 'decrease' && quantity > 1) {
+      this.setState((previousState) => ({
+        quantity: previousState.quantity - 1,
+      }));
+    }
   }
 
   render() {
-    const { quantity, totalPrice } = this.state;
+    const { quantity } = this.state;
     const { cart: { title, price } } = this.props;
     return (
       <div>
         <h3 data-testid="shopping-cart-product-name">
           {title}
         </h3>
-        <p>{price}</p>
+        <p>
+          Preço de cada um:
+          {price}
+        </p>
         <p data-testid="shopping-cart-product-quantity">
           Quantidade:
           {quantity}
         </p>
         <p>
           Preço total:
-          {totalPrice}
+          {quantity * price}
         </p>
-        <button type="button">+</button>
-        <button type="button">-</button>
+        <button
+          type="button"
+          data-testid="product-increase-quantity"
+          onClick={ () => { this.handleClick('increase'); } }
+        >
+          +
+        </button>
+        <button
+          type="button"
+          data-testid="product-decrease-quantity"
+          onClick={ () => { this.handleClick('decrease'); } }
+        >
+          -
+        </button>
       </div>
     );
   }
