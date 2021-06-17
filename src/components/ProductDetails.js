7ef86020d98442bf7cbd8a8e2e9c534f40b15955
Vinicius <viniciusfod@gmail.com>
2021-06-17 19:37:36 -0300

    11req-main-group-12-form-details

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import Rating from './Rating';
 
 export default class ProductDetails extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      email: '',
+      message: '',
+      rating: 0,
+      showForm: false,
+    };
 
     this.setItemStorage = this.setItemStorage.bind(this);
+    this.inputHandler = this.inputHandler.bind(this);
+    this.renderRating = this.renderRating.bind(this);
   }
 
   componentDidMount() {
@@ -29,9 +38,21 @@ export default class ProductDetails extends Component {
     localStorage.setItem('productInfos', JSON.stringify(productInfo));
   }
 
+  inputHandler({ target: { name, value } }) {
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  renderRating() {
+    this.setState({
+      showForm: true,
+    });
+  }
+
   render() {
     const { location: { state: { title, thumbnail, price } } } = this.props;
-
+    const { email, message, rating, showForm } = this.state;
     return (
       <div>
         <Link to="/cart">
@@ -50,6 +71,77 @@ export default class ProductDetails extends Component {
         >
           Adicionar ao carrinho
         </button>
+        <form>
+          <input
+            name="email"
+            required
+            placeholder="Email"
+            type="text"
+            value={ email }
+            onChange={ this.inputHandler }
+          />
+          <textarea
+            name="message"
+            placeholder="Message"
+            data-testid="product-detail-evaluation"
+            cols="30"
+            rows="10"
+            value={ message }
+            onChange={ this.inputHandler }
+          />
+          <label htmlFor="rating">
+            1
+            <input
+              type="radio"
+              id="1"
+              name="rating"
+              value="1"
+              onChange={ this.inputHandler }
+            />
+          </label>
+          <label htmlFor="rating">
+            2
+            <input
+              type="radio"
+              id="2"
+              name="rating"
+              value="2"
+              onChange={ this.inputHandler }
+            />
+          </label>
+          <label htmlFor="rating">
+            3
+            <input
+              type="radio"
+              id="3"
+              name="rating"
+              value="3"
+              onChange={ this.inputHandler }
+            />
+          </label>
+          <label htmlFor="rating">
+            4
+            <input
+              type="radio"
+              id="4"
+              name="rating"
+              value="4"
+              onChange={ this.inputHandler }
+            />
+          </label>
+          <label htmlFor="rating">
+            5
+            <input
+              type="radio"
+              id="5"
+              name="rating"
+              value="5"
+              onChange={ this.inputHandler }
+            />
+          </label>
+          <button onClick={ this.renderRating } type="button">Avaliar</button>
+        </form>
+        { showForm ? <Rating email={ email } msg={ message } rating={ rating } /> : null}
       </div>
     );
   }
---
+++ src/components/Rating.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Rating extends Component {
+  constructor(props) {
+    super(props);
+    const { email, msg, rating } = this.props;
+    this.state = {
+      email,
+      message: msg,
+      rating,
+    };
+  }
+
+  render() {
+    const { email, message, rating } = this.state;
+    return (
+      <div>
+        <strong>{ email }</strong>
+        <p>{ message }</p>
+        <p>{ rating }</p>
+      </div>
+    );
+  }
+}
+
+Rating.propTypes = {
+  email: PropTypes.string,
+  message: PropTypes.string,
+  rating: PropTypes.number,
+}.isRequired;
+
+export default Rating;
