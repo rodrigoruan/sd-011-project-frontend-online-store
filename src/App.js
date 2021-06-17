68053c41cadc63c3e2279a453296c2add44126f4
Matheus Costa <mathws.costag@gmail.com>
2021-06-17 02:51:22 -0300

    arruma o lint

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import Home from './components/Home';
-import './styles/App.css';
 
 class App extends Component {
   render() {
