23da90ed642ff062a1aae819df46272c669bf252
camiloLelis <lafirmafuerte@gmail.com>
2021-06-17 19:24:22 -0300

    requisito-7-atendido

--- src/App.js
+++ src/App.js
@@ -3,20 +3,24 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 // Páginas
 import Main from './pages/Main';
 import ShoppingCart from './pages/ShoppingCart';
+import ProductDetails from './pages/ProductDetails';
 // Componentes
 import CategoryFilter from './components/CategoryFilter';
 import './App.css';
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={ Main } />
-        <Route exact path="/shoppingcart" component={ ShoppingCart } />
-        <Route exact path="/categoryfilter" component={ CategoryFilter } />
-      </Switch>
-    </BrowserRouter>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={ Main } />
+          <Route exact path="/shoppingcart" component={ ShoppingCart } />
+          <Route exact path="/categoryfilter" component={ CategoryFilter } />
+          <Route path="/detalhes/:id" component={ ProductDetails } />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
