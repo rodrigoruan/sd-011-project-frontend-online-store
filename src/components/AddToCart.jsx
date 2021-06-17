00aeaf88cdba2adce834fc7d77f9365ff71acbe8
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-06-17 18:56:36 -0300

    quantidade de clicks armazenada no localstorage

--- src/components/AddToCart.jsx
+++ src/components/AddToCart.jsx
@@ -12,12 +12,22 @@ class AddToCart extends React.Component {
     const { item } = this.props;
     let existingCart = JSON.parse(localStorage.getItem('items'));
     if (existingCart == null) existingCart = [];
-    existingCart.push(item);
+    let existingQty = JSON.parse(localStorage.getItem(item.product.id));
+    if (existingQty == null) existingQty = 1;
+    const existingItem = existingCart.find(
+      (currItem) => currItem.product.id === item.product.id,
+    );
+    if (existingItem === undefined) {
+      existingCart.push(item);
+    } else { existingQty += 1; }
     localStorage.setItem('items', JSON.stringify(existingCart));
+    localStorage.setItem(item.product.id, JSON.stringify(existingQty));
   }
 
   readLocalStorage() {
+    const { item } = this.props;
     console.log(JSON.parse(localStorage.getItem('items')));
+    console.log(JSON.parse(localStorage.getItem(item.product.id)));
   }
 
   render() {
@@ -30,6 +40,13 @@ class AddToCart extends React.Component {
         >
           Adicionar ao Carrinho
         </button>
+        <button
+          type="button"
+          data-testid="product-add-to-cart"
+          onClick={ () => this.readLocalStorage() }
+        >
+          teste
+        </button>
       </div>
     );
   }
@@ -38,5 +55,5 @@ class AddToCart extends React.Component {
 export default AddToCart;
 
 AddToCart.propTypes = {
-  item: PropTypes.objectOf().isRequired,
+  item: PropTypes.objectOf(PropTypes.any).isRequired,
 };
