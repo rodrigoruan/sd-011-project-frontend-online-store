8a13db92f3e411245364ca1ba0c9a49cf6b55fcf
Rafael Cavalcante <rafael.cavalcante.es@gmail.com>
2021-06-17 23:13:08 -0300

    adiciona prço total ao pe da pagina do carrinho

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -3,12 +3,23 @@ import PropTypes from 'prop-types';
 import { CartItem } from '../components';
 
 class Cart extends React.Component {
+  getTotalPrice() {
+    const { productList } = this.props;
+    if (productList.length) {
+      return productList.reduce((acc, current) => {
+        acc += (current.price * current.quantity);
+        return acc;
+      }, 0).toFixed(2);
+    }
+    return 0;
+  }
+
   render() {
     const { productList, removeItemFromCart, updateQuantity } = this.props;
     return (
       <main>
         <h1>Carrinho de Compras</h1>
-        {!productList
+        {!productList.length
           ? (
             <div data-testid="shopping-cart-empty-message">
               <p>Seu carrinho está vazio</p>
@@ -25,7 +36,11 @@ class Cart extends React.Component {
               ))}
             </ul>
           )}
-        <h2>{ }</h2>
+        <h2>
+          {' '}
+          VALOR TOTAL : R$
+          { this.getTotalPrice() }
+        </h2>
       </main>
     );
   }
