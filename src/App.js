236fa0917d9558c8a012b2702fb20101018b3550
Luciano Pimenta <l.a.p.pimenta.j@gmail.com>
2021-06-18 16:28:05 -0300

    Atualizacoes e Code Review

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './App.css';
-// import * as api from './services/api';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import SearchPageHome from './components/SearchPageHome';
 import ShoppingCart from './components/ShoppingCart';
