c8ffdc3b2ad6c80999b83edc238f4fa84174a989
Matheus Antonio da Silva <matheusantonio232@gmail.com>
2021-06-18 17:49:24 -0300

    Redirecionando objeto produto para details

--- src/Components/ItemCard.js
+++ src/Components/ItemCard.js
@@ -5,13 +5,19 @@ import PropTypes from 'prop-types';
 class ItemCard extends Component {
   render() {
     const { price, title, id, thumbnail } = this.props;
+    const product = { price, title, id, thumbnail };
     return (
       <li data-testid="product" className="product-card">
         <h4 className="product-card-title">{title}</h4>
         <img className="product-image" alt="imagem do produto" src={ thumbnail } />
         <p className="product-card-price">{ `R$ ${price}` }</p>
         <p className="product-link-details">
-          <Link to={ `/product/${id}` }>
+          <Link
+            to={ {
+              pathname: `/product/${id}`,
+              state: { product },
+            } }
+          >
             Ver Detalhes
           </Link>
         </p>
