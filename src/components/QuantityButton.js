222c24d65ae728bb1c007f5aaf2fe6d3b7e68ccb
Kevin Willian <77885339+zLibrian@users.noreply.github.com>
2021-06-18 16:57:39 -0300

    Update src/components/QuantityButton.js
    Co-authored-by: Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>

--- src/components/QuantityButton.js
+++ src/components/QuantityButton.js
@@ -36,8 +36,8 @@ class QuantityButton extends Component {
   }
 
   subtractQuantity() {
-    this.setState((prevState) => ({
-      quantity: prevState.quantity - 1,
+    this.setState(({ quantity }) => ({
+      quantity: quantity - 1,
     }), () => {
       const { product } = this.props;
       const products = JSON.parse(localStorage.getItem('products'));
