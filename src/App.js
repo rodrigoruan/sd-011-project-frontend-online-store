2ce02309b737a133c1a13758505c5564bb27eefe
Júlio Mota <juliomotacampos@gmail.com>
2021-06-18 18:19:05 -0300

    Primeiro commit dessa branch

--- src/App.js
+++ src/App.js
@@ -3,14 +3,9 @@ import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Home from './Pages/Home';
 import ShoppingCart from './Pages/ShoppingCart';
 import ProductDetail from './Pages/ProductDetail/ProductDetail';
-// import * as api from './services/api';
-// test
 
 class App extends Component {
   render() {
-    // api.getCategories().then(categories => { console.log(categories) });
-    // api.getProductsFromCategoryAndQuery('MLB1055', 'Motorola').then(categories => { console.log(categories) });
-
     return (
       <BrowserRouter>
         <Switch>
