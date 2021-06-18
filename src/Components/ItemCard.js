a98479e10f3233f84355854181e7d7276e36e02e
gab-boff <gmeninboff@gmail.com>
2021-06-18 17:50:05 -0300

    a

--- src/Components/ItemCard.js
+++ src/Components/ItemCard.js
@@ -1,21 +1,21 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 class ItemCard extends Component {
   render() {
     const { price, title, id, thumbnail } = this.props;
     return (
-      <li data-testid="product" className="product-card">
-        <h4 className="product-card-title">{title}</h4>
-        <img className="product-image" alt="imagem do produto" src={ thumbnail } />
-        <p className="product-card-price">{ `R$ ${price}` }</p>
-        <p className="product-link-details">
-          <Link to={ `/product/${id}` }>
+      <Link to={ `/product/${id}` }>
+        <li data-testid="product" className="product-card">
+          <h4 className="product-card-title">{title}</h4>
+          <img className="product-image" alt="imagem do produto" src={ thumbnail } />
+          <p className="product-card-price">{`R$ ${price}`}</p>
+          <p className="product-link-details">
             Ver Detalhes
-          </Link>
-        </p>
-      </li>
+          </p>
+        </li>
+      </Link>
     );
   }
 }
