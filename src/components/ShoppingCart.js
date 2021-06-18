033cd642fdae4b09b03538eadc87fd02aa3428c6
Cesar Cançado Bhering <cesarcbhering@gmail.com>
2021-06-18 16:33:24 -0300

    LINT corrigido - 10 Requisitos OK

--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -15,9 +15,17 @@ export default class ShoppingCart extends Component {
     };
   }
 
+  componentWillUnmount() {
+    const { items, quantity } = this.state;
+    localStorage.setItem('items', JSON.stringify(items));
+    localStorage.setItem('quantity', JSON.stringify(quantity));
+  }
+
   increaseQuantity(event) {
-    let { quantity } = this.state;
-    const { target: { name, id } } = event;
+    const { quantity } = this.state;
+    const {
+      target: { name, id },
+    } = event;
     quantity[name][id] += 1;
     this.setState({
       quantity,
@@ -25,9 +33,13 @@ export default class ShoppingCart extends Component {
   }
 
   decreaseQuantity(event) {
-    let { quantity } = this.state;
-    const { target: { name, id } } = event;
-    if (quantity[name][id] >= 1) {quantity[name][id] -= 1};
+    const { quantity } = this.state;
+    const {
+      target: { name, id },
+    } = event;
+    if (quantity[name][id] >= 1) {
+      quantity[name][id] -= 1;
+    }
     this.setState({
       quantity,
     });
@@ -35,15 +47,12 @@ export default class ShoppingCart extends Component {
 
   removeAllItems(event) {
     const { items } = this.state;
-    const { target: { id } } = event;
+    const {
+      target: { id },
+    } = event;
     this.setState({
       items: items.filter((product) => product.id !== id),
     });
-  }
-
-  componentWillUnmount() {
-    localStorage.setItem('items', JSON.stringify(this.state.items));
-    localStorage.setItem('quantity', JSON.stringify(this.state.quantity));
   }
 
   render() {
@@ -51,45 +60,54 @@ export default class ShoppingCart extends Component {
     const { items, quantity } = this.state;
     return (
       <div>
-        { items.map(({ product: { title, thumbnail, price, id } }, index) => (
-          totalPrice += price *quantity[index][id],
-          <div key={ index } data-testid="shopping-cart-product-name">
-            <img
-              src={ closeButton }
-              alt="close button"
-              id={ id }
-              onClick={ this.removeAllItems }
-            />
-            <img src={ thumbnail } alt="Foto do Produto" />
-            <p>{title}</p>
-            <button
-              name={ index }
-              id={ id }
-              type="button"
-              data-testid="product-decrease-quantity"
-              onClick={ this.decreaseQuantity }
-            >
-              -
-            </button>
-            <p data-testid="shopping-cart-product-quantity">
-              { `Quantidade: ${quantity[index][id]}` }
-            </p>
-            <button
-              name={ index }
-              id={ id }
-              type="button"
-              data-testid="product-increase-quantity"
-              onClick={ this.increaseQuantity }
-            >
-              +
-            </button>
-            <p>{`Preço: R$${price}`}</p>
-          </div>
-        )) }
-        {items.length === 0
-          ? <p data-testid="shopping-cart-empty-message">Seu carrinho está vazio</p>
-          : <p />}
-          <p>{ `Valor total: R$${totalPrice.toFixed(2)}` }</p>
+        {items.map(
+          ({ product: { title, thumbnail, price, id } }, index) => {
+            totalPrice += price * quantity[index][id];
+            return (
+              <div key={ index } data-testid="shopping-cart-product-name">
+                <button type="button" onClick={ this.removeAllItems }>
+                  <img
+                    src={ closeButton }
+                    alt="close button"
+                    id={ id }
+                  />
+                </button>
+                <img src={ thumbnail } alt="Foto do Produto" />
+                <p>{title}</p>
+                <button
+                  name={ index }
+                  id={ id }
+                  type="button"
+                  data-testid="product-decrease-quantity"
+                  onClick={ this.decreaseQuantity }
+                >
+                  -
+                </button>
+                <p data-testid="shopping-cart-product-quantity">
+                  {`Quantidade: ${quantity[index][id]}`}
+                </p>
+                <button
+                  name={ index }
+                  id={ id }
+                  type="button"
+                  data-testid="product-increase-quantity"
+                  onClick={ this.increaseQuantity }
+                >
+                  +
+                </button>
+                <p>{`Preço: R$${price}`}</p>
+              </div>
+            );
+          },
+        )}
+        {items.length === 0 ? (
+          <p data-testid="shopping-cart-empty-message">
+            Seu carrinho está vazio
+          </p>
+        ) : (
+          <p />
+        )}
+        <p>{`Valor total: R$${totalPrice.toFixed(2)}`}</p>
       </div>
     );
   }
