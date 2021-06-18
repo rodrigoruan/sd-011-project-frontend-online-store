e07d70382a8b24f9fd502488afd6bb4c0f064859
Gisele Costa <jikinha@gmail.com>
2021-06-18 16:36:01 -0300

    Requerimento 12 "concluído"

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Home from './pages/Home';
 import Cart from './pages/Cart';
 import Details from './pages/Details';
+import Checkout from './pages/Checkout';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route exact path="/" component={ Home } />
       <Route path="/cart" component={ Cart } />
       <Route path="/details/:id" render={ (props) => <Details { ...props } /> } />
+      <Route path="/checkout" component={ Checkout } />
     </BrowserRouter>
   );
 }
