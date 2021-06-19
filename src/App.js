ef4a1d0270aa4e5dbf3647c8a8a125a05999117d
Bruno Duarte <brunoarduarte@gmail.com>
2021-06-18 22:03:01 -0300

    Requisito 12

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Cart from './Components/Cart';
+import { Checkout } from './Components/Checkout';
 import { Product } from './Components/Product';
 import Home from './Components/Home';
 
@@ -15,6 +16,7 @@ function App() {
           </Route>
           <Route path="/cart" component={ Cart } />
           <Route path="/product/:id" render={ (props) => <Product { ...props } /> } />
+          <Route path="/checkout" component={ Checkout } />
         </Switch>
       </BrowserRouter>
     </div>
