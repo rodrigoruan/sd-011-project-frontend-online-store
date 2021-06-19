7273059f6d6b9c9011498cdb7be3400d87cef2a2
Julio Filizzola <juliofilizzola@hotmail.com>
2021-06-19 15:59:19 -0300

    [28]=importando bootstrap no componente button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 
 export default class Button extends Component {
   render() {
