68519d1a22a8154c1f74cbaed991ad8980c57775
Marco Galindo <marco1408@gmail.com>
2021-06-19 16:58:12 -0300

    css dos produtos

--- src/components/ProductListSection/ProductListSection.jsx
+++ src/components/ProductListSection/ProductListSection.jsx
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import styles from './ProductListSection.module.css';
 
 export default class ProductListSection extends Component {
   render() {
     const { product: { title, price, thumbnail } } = this.props;
     return (
-      <div data-testid="product">
-        <img src={ thumbnail } alt={ title } />
+      <div data-testid="product" className={ styles.product }>
+        <img src={ thumbnail } alt={ title } className={ styles.productImage } />
         <h3>{title}</h3>
-        <span>{price}</span>
-        <button type="button">Adicionar ao carrinho</button>
-        <button type="button">Detalhes</button>
+        <span>{`R$ ${price}`}</span>
+        <button className={ styles.cartBtn } type="button">Adicionar ao carrinho</button>
+        <button className={ styles.detailsBtn } type="button">Detalhes</button>
       </div>
     );
   }
