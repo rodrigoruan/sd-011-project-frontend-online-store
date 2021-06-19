1e338a064bc70186ac0e1a32375391270d460c54
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-06-19 14:39:26 -0300

    criando logica do componente AddAndRemoveCartItem

--- src/components/AddAndRemoveCartItem.js
+++ src/components/AddAndRemoveCartItem.js
@@ -1,9 +1,37 @@
-// import React, { Component } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-// export default class AddAndRemoveCartItem extends Component {
-//   render() {
-//     return(
+export default class AddAndRemoveCartItem extends Component {
+  render() {
+    const { dataTestId, operator, increase, decrease } = this.props;
 
-//     );
-//   }
-// }
+    if (operator === '-') {
+      return (
+        <button
+          data-testid={ dataTestId }
+          type="button"
+          onClick={ decrease }
+        >
+          -
+        </button>
+      );
+    }
+
+    return (
+      <button
+        data-testid={ dataTestId }
+        type="button"
+        onClick={ increase }
+      >
+        +
+      </button>
+    );
+  }
+}
+
+AddAndRemoveCartItem.propTypes = {
+  dataTestId: PropTypes.string.isRequired,
+  operator: PropTypes.string.isRequired,
+  increase: PropTypes.func.isRequired,
+  decrease: PropTypes.func.isRequired,
+};
