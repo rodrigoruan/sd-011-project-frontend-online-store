3b0e217e062cdd5b1e81d333aef383a6b8f42674
Dário <dario.junior.rs@gmail.com>
2021-06-18 23:54:47 -0300

    Improvements: CSS and some alterations in page

--- src/components/ProductCard/CardItem.jsx
+++ src/components/ProductCard/CardItem.jsx
@@ -11,38 +11,48 @@ export default class CardItem extends Component {
       } } = this.props;
     return (
       <div className="CardContainer" data-testid="product">
-        <h3>{title}</h3>
-        <img src={ thumbnail } alt="Imagem do produto" className="Thumbnail" />
-        <p className="pPrice">
-          {price}
-          <span>
-            {currencyId}
-          </span>
-        </p>
-        <Link
-          to={ {
-            pathname: `/product/${id}`,
-            state: {
-              name: title,
-              image: thumbnail,
-              price: { total: price, currency: currencyId },
-              spec: attributes,
-            },
-          } }
-          data-testid="product-detail-link"
-        >
-          Mais detalhes
-        </Link>
-        <AddToCartBttn
-          product={ {
-            id,
-            title,
-            price,
-            thumbnail,
-            currency_id: currencyId,
-          } }
-          dataTest="product-add-to-cart"
-        />
+        <div className="titleContainer">{title}</div>
+        <div className="thumbContainer">
+          <img src={ thumbnail } alt="Imagem do produto" className="Thumbnail" />
+        </div>
+        <div className="priceContainer">
+          R$
+          { price }
+        </div>
+        <div className="ratingContainer">
+          ⭐⭐⭐⭐(
+          {
+            parseInt(Math.random() * 100, 10)
+          }
+          )
+        </div>
+        <div className="addToCartAndDetailsContainer">
+          <Link
+            className="moreDetails"
+            to={ {
+              pathname: `/product/${id}`,
+              state: {
+                name: title,
+                image: thumbnail,
+                price: { total: price, currency: currencyId },
+                spec: attributes,
+              },
+            } }
+            data-testid="product-detail-link"
+          >
+            Mais detalhes
+          </Link>
+          <AddToCartBttn
+            product={ {
+              id,
+              title,
+              price,
+              thumbnail,
+              currency_id: currencyId,
+            } }
+            dataTest="product-add-to-cart"
+          />
+        </div>
       </div>
     );
   }
