6395a60f11702839d3c5ec0027f1e3c80150f858
IeiriMauricio <mauricio.s.ieiri@gmail.com>
2021-06-20 20:14:51 -0300

    Requisito 10 completo

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -3,9 +3,38 @@ import { Link } from 'react-router-dom';
 import CartItem from '../components/CartItem';
 
 class Cart extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      totalPrice: 0,
+    };
+
+    this.updateTotalPrice = this.updateTotalPrice.bind(this);
+  }
+
+  componentDidMount() {
+    this.updateTotalPrice();
+  }
+
   getProductFromLocalStorage() {
     const cart = localStorage.getItem('cart');
     return JSON.parse(cart);
+  }
+
+  saveToLocalStorage(totalPrice) {
+    localStorage.setItem('totalPrice', JSON.stringify(totalPrice));
+  }
+
+  updateTotalPrice() {
+    const localCart = this.getProductFromLocalStorage();
+    const productPrices = localCart.map((product) => Math
+      .round((product.quantity * product.price) * 100) / 100);
+    const totalPrice = productPrices.reduce((acc, current) => acc + current);
+    this.setState({
+      totalPrice,
+    });
+    this.saveToLocalStorage(totalPrice);
+    return totalPrice;
   }
 
   renderLocalStorageItems() {
@@ -16,16 +45,25 @@ class Cart extends React.Component {
       );
     }
     return oldCart.map((product) => (
-      <CartItem key={ product.id } product={ product } />
+      <CartItem
+        key={ product.id }
+        product={ product }
+        updateTotalPrice={ this.updateTotalPrice }
+      />
     ));
   }
 
   render() {
+    const { totalPrice } = this.state;
     return (
       <div>
         <Link to="/">Voltar</Link>
         { this.renderLocalStorageItems() }
-        <p>Soma Total</p>
+        <p>
+          Soma Total: R$
+          { totalPrice }
+        </p>
+        {/* button para finalizar a compra, enviar ao localStorage o valor total */}
         <Link to="/">Voltar Para Pagina Inicial</Link>
       </div>
     );
