e5412166b73284cc2efc22f0d6962dc8ba95332f
Julio Filizzola <juliofilizzola@hotmail.com>
2021-06-20 18:55:20 -0300

    [28]=editando About 1

--- src/components/About.js
+++ src/components/About.js
@@ -10,7 +10,11 @@ export default class About extends Component {
         <h1>Sobre nós</h1>
         <p>Esse site foi criado todo com React, e baseado na api do Mercado Livre.</p>
         <p>Foi Feito em grupo, em um projeto da Trybe!</p>
-        <p>Grupo composto por: Julio Filizzola, Luciano Pimenta. Alan Conçalves e Gabriel Carvalho. </p>
+        <p>
+          Grupo composto por: Julio Filizzola,
+          Luciano Pimenta. Alan Conçalves e Gabriel Carvalho.
+          {' '}
+        </p>
 
       </div>
     );
