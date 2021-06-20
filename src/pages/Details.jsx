84aab63b1242b526794364cff20d794dd954df7f
Víctor Faria <vitilevi@hotmail.com>
2021-06-20 19:48:21 -0300

    atualizado

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import AvaliationForm from '../components/AvaliationForm';
 import Avaliations from '../components/Avaliations';
 import handleCart from '../services/localStorage';
+import '../styles/Details.css';
 
 export default class Details extends Component {
   constructor() {
@@ -44,43 +45,56 @@ export default class Details extends Component {
     const { cart } = this.state;
     const { free_shipping: freeShipping } = shipping;
     return (
-      <div>
-        <div>
-          <Link to="/ShoppingCart" data-testid="shopping-cart-button">
-            Carrinho
-            <span data-testid="shopping-cart-size">{ cart }</span>
+      <div className="details">
+        <div className="detail-buttons">
+          <Link to="/">
+            <div className="icons">
+              <i className="fas fa-arrow-left">Voltar</i>
+            </div>
+          </Link>
+          <Link
+            to="/ShoppingCart"
+            data-testid="shopping-cart-button"
+            className="btn btn-warning"
+          >
+            <i className="fas fa-shopping-cart" />
+            <span className="cart-size" data-testid="shopping-cart-size">
+              { cart }
+            </span>
           </Link>
         </div>
         <div>
           <div>
-            <span data-testid="product-detail-name">{`${title}, ${price}`}</span>
+            <h3 data-testid="product-detail-name">{`${title}`}</h3>
             {
               freeShipping
                 ? <span data-testid="free-shipping">Frete Grátis</span>
                 : null
             }
           </div>
-          <div>
-            <img src={ thumbnail } alt={ title } />
-          </div>
-          <div>
-            <h3>Especificações técnicas</h3>
-            { attributes.map(({ name, value_name: valueName, id: attributeId }) => (
-              <p key={ attributeId }>
-                { name }
-                :
-                { valueName }
-              </p>
-            ))}
-          </div>
-          <div>
-            <button
-              type="button"
-              data-testid="product-detail-add-to-cart"
-              onClick={ () => this.addQuantity(element) }
-            >
-              Adicionar ao carrinho
-            </button>
+          <div className="detail-image-info">
+            <div className="detail-image">
+              <img src={ thumbnail } alt={ title } />
+            </div>
+            <div className="detail-information">
+              <h3>Especificações técnicas</h3>
+              { attributes.map(({ name, value_name: valueName, id: attributeId }) => (
+                <p key={ attributeId }>
+                  { name }
+                  :
+                  { valueName }
+                </p>
+              ))}
+              <p className="detail-price">{`R$${price}`}</p>
+              <button
+                type="button"
+                className="btn btn-success"
+                data-testid="product-detail-add-to-cart"
+                onClick={ () => this.addQuantity(element) }
+              >
+                Adicionar ao carrinho
+              </button>
+            </div>
           </div>
         </div>
 
