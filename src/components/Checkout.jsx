814f11e2209a5af4a7873cf08447e3421bbffec3
Daniel <danielfdoficial@gmail.com>
2021-06-21 12:38:33 -0300

    finaliza requisito 12, formulario encaminha para a pagina inicial

--- src/components/Checkout.jsx
+++ src/components/Checkout.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import '../CSS/Checkout.css';
-
-// Muitas coisas foram comentadas para o componente ser renderizado. Terminar o requisito 12 amanhã, lembrar de colocar a handleState() em todos os inputs que refenciam o state. Lembrar também de limpar o estado após submitar o formulário.
+import Cardcheckout from './Cardcheckout';
 
 export default class Checkout extends Component {
   constructor() {
     super();
     this.state = {
       cartProducts: JSON.parse(localStorage.getItem('cart')),
+      redirect: false,
       name: '',
       cpf: '',
       email: '',
@@ -18,14 +18,11 @@ export default class Checkout extends Component {
       complemento: '',
       number: 0,
       city: '',
-      // estado: '',
-      // payOption: false,
     };
 
-    this.buildCard = this.buildCard.bind(this);
     this.total = this.total.bind(this);
     this.handleState = this.handleState.bind(this);
-    this.clearState = this.clearState.bind(this);
+    this.clearStorage = this.clearStorage.bind(this);
   }
 
   handleState({ target }) {
@@ -36,33 +33,13 @@ export default class Checkout extends Component {
     });
   }
 
-  clearState() {
-    /* this.setState({
-      name: '',
-      cpf: '',
-      email: '',
-      phone: '',
-      cep: '',
-      address: '',
-      complemento: '',
-      number: 0,
-      city: '',
-      estado: '',
-    }); */
-    alert('Olá');
-  }
-
-  buildCard() {
-    const { cartProducts } = this.state;
-    return cartProducts.map((product) => (
-      <div key={ product.id } className="checkout-product-card">
-        <img src={ product.thumbnail } alt="imagem" width="220px" className="checkout-product-img" />
-        <Link to="/Cart" className="checkout-product-card-link">
-          <p className="checkout-product-title">{product.title}</p>
-        </Link>
-        <p className="checkout-product-price">{`R$ ${product.price}` }</p>
-      </div>
-    ));
+  clearStorage(event) {
+    event.preventDefault();
+    localStorage.removeItem('cart');
+    alert('Compra concluída com sucesso! Volte sempre!');
+    this.setState({
+      redirect: true,
+    });
   }
 
   total() {
@@ -223,20 +200,18 @@ export default class Checkout extends Component {
   }
 
   render() {
+    const { redirect } = this.state;
+    if (redirect) {
+      return <Redirect to="/" />;
+    }
     return (
       <div className="checkout-div">
         <Link to="/Cart" className="checkout-cart-link">voltar para o carrinho</Link>
         <div className="checkout-main-content">
-          <div className="checkout-products-list">
-            <h3>Revise seus Produtos</h3>
-            <div>
-              {this.buildCard()}
-            </div>
-
-          </div>
+          <Cardcheckout />
           <div className="checkout-forms">
-            <form>
-              <p className="checkout-total-price">{`Total a pagar: ${this.total()}`}</p>
+            <form onSubmit={ this.clearStorage }>
+              <p className="checkout-total-price">{`Total a pagar: R$${this.total()}`}</p>
               <h3>Informações do Comprador</h3>
               <fieldset>
                 {this.renderNameInput()}
@@ -245,7 +220,7 @@ export default class Checkout extends Component {
               <fieldset>
                 {this.renderPaymentInput()}
               </fieldset>
-              <button type="submit" onClick={ this.clearState }>Comprar</button>
+              <button type="submit">Comprar</button>
             </form>
           </div>
         </div>
