7eb6e271d89b652dc679b155ad202f761c74fa29
Lais Simenikim <l.simenikim@gmail.com>
2021-06-21 18:50:30 -0300

    parte requisito 9

--- src/pages/ProductDetail.js
+++ src/pages/ProductDetail.js
@@ -2,18 +2,44 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 class ProductDetail extends React.Component {
+  onAddToCart= () => {
+    const { onAddToCart } = this.props;
+    const { location } = this.props;
+    const { state } = location;
+    const { product } = state;
+
+    onAddToCart(product);
+  }
+
   render() {
     const { location } = this.props;
     const { state } = location;
     const { product } = state;
-    return <div data-testid="product-detail-name">{ product.title }</div>;
+
+    return (
+      <div data-testid="product-detail-name">
+        <h2>{ product.title }</h2>
+        <button
+          type="button"
+          data-testid="product-detail-add-to-cart"
+          onClick={ this.onAddToCart }
+        >
+          Adicionar no Carrinho
+        </button>
+      </div>
+    );
   }
 }
 
 ProductDetail.propTypes = {
-  location: PropTypes.object,
-  state: PropTypes.object,
-  product: PropTypes.object,
-}.isRequired;
+  location: PropTypes.shape({
+    state: PropTypes.shape({
+      product: PropTypes.shape({
+        title: PropTypes.string.isRequired,
+      }).isRequired,
+    }).isRequired,
+  }).isRequired,
+  onAddToCart: PropTypes.func.isRequired,
+};
 
 export default ProductDetail;
