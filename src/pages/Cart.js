45b933954c08f8c570a22fdbacf5ce9253305351
marcoswarmling <marcoswarmling15@gmail.com>
2021-06-21 14:32:08 -0300

    teste 2

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,45 +1,26 @@
 import React from 'react';
-import CartCard from '../components/CartCard';
 
 export default class Cart extends React.Component {
-  constructor() {
-    super();
-    this.getAllValues = this.getAllValues.bind(this);
-  }
-
-  getAllValues() {
-    const items = { ...localStorage };
-    const totalValues = Object.values(items).reduce((e, e1) => {
-      const valuesIndex = JSON.parse(e1);
-      return e + valuesIndex.totalValues;
-    }, 0);
-    return (
-      <div>
-        Valor total:
-        {' '}
-        {totalValues}
-      </div>
-    );
-  }
+  // constructor() {
+  //   super();
+  // }
 
   render() {
     const items = { ...localStorage };
     const text = 'shopping-cart-empty-message';
     const renderCart1 = Object.values(items).map((e) => (
       <div key={ JSON.parse(e).id }>
-        <CartCard { ...JSON.parse(e) } />
-        <div />
+        <p data-testid="shopping-cart-product-name">{ JSON.parse(e).title }</p>
+        <img src={ JSON.parse(e).thumbnail } alt={ e } />
+        <p data-testid="shopping-cart-product-quantity">
+          { JSON.parse(e).quantity }
+        </p>
       </div>
-
     ));
     return (
       <div>
         { window.localStorage
           .length > 0 ? renderCart1 : <p data-testid={ text }>Seu carrinho está vazio</p>}
-        <div>
-          <br />
-          {this.getAllValues()}
-        </div>
       </div>
     );
   }
