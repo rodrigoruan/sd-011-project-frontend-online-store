b5370a1a65ef7b4382b9f5b1d229e50607a9c449
Mauricio Ruviaro <mauricioruviaro07@gmail.com>
2021-06-24 15:34:22 -0300

    requisito 12 resolvido

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -2,10 +2,43 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
 export default class Checkout extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      fullName: '',
+      email: '',
+      cpf: '',
+      phone: '',
+      zipCode: '',
+      adress: '',
+    };
+  }
+
+  handleChange = ({ target }) => {
+    const { name, value } = target;
+    this.setState({
+      [name]: value,
+    });
+  }
+
+  resetState = () => {
+    this.setState({
+      fullName: '',
+      email: '',
+      cpf: '',
+      phone: '',
+      zipCode: '',
+      adress: '',
+    });
+  }
+
   render() {
     const { location } = this.props;
     const { state } = location;
     const { allProducts } = state;
+    const { fullName, email, cpf, phone, zipCode, adress } = this.state;
+
     return (
       <div>
         <div>
@@ -19,37 +52,55 @@ export default class Checkout extends Component {
         </div>
         <form>
           <input
+            onChange={ this.handleChange }
+            name="fullName"
+            value={ fullName }
             data-testid="checkout-fullname"
             type="text"
             placeholder="Nome completo"
           />
           <input
+            onChange={ this.handleChange }
+            name="email"
+            value={ email }
             data-testid="checkout-email"
             type="text"
             placeholder="Email"
           />
           <input
+            onChange={ this.handleChange }
+            name="cpf"
+            value={ cpf }
             data-testid="checkout-cpf"
             type="text"
             placeholder="Cpf"
           />
           <input
+            onChange={ this.handleChange }
+            name="phone"
+            value={ phone }
             data-testid="checkout-phone"
             type="text"
             placeholder="Telefone"
           />
           <input
+            onChange={ this.handleChange }
+            name="zipCode"
+            value={ zipCode }
             data-testid="checkout-cep"
             type="text"
             placeholder="CEP"
           />
           <input
+            onChange={ this.handleChange }
+            name="adress"
+            value={ adress }
             data-testid="checkout-address"
             type="text"
             placeholder="Endereço"
           />
         </form>
-        <button type="button">Comprar</button>
+        <button onClick={ this.resetState } type="button">Comprar</button>
       </div>
     );
   }
