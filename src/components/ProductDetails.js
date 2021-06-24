a8d6d4733251ab0ac5d89cbbf03a516815e987a1
rodolfowolff <rodolfoluizwolff@gmail.com>
2021-06-24 14:24:03 -0300

    Adiciona requisito 15

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -32,7 +32,7 @@ class ProductDetails extends React.Component {
     const { product: { id, title, thumbnail, price }, quantity } = this.state;
     const previousList = this.loadCartList();
     if (previousList[id]) {
-      for (let i = 0; i < quantity; i += 1) {
+      for (let i = 0;i < quantity;i += 1) {
         previousList[id].quantity += 1;
       }
     } else {
@@ -104,7 +104,7 @@ class ProductDetails extends React.Component {
   render() {
     const { product:
       { title, thumbnail, price },
-    quantity, cartQuantity } = this.state;
+      quantity, cartQuantity } = this.state;
 
     return (
       <div>
@@ -119,7 +119,7 @@ class ProductDetails extends React.Component {
         </header>
         <h3 data-testid="product-detail-name">
           { title }
-          {' - R$ '}
+          { ' - R$ ' }
           { price === undefined ? price : parseFloat(price).toFixed(2).replace('.', ',') }
         </h3>
         <div className="container-image-and-product-details">
