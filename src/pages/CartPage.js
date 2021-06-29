765c37f5c35c36730eab57842b214d15223e5024
Luiz Alexandre Wendel Balbino <alexandre.lawb@gmail.com>
2021-06-29 19:59:39 -0300

    Update src/pages/CartPage.js
    Co-authored-by: Tales Coelho dos Santos <tales-coelho@live.com>

--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -8,10 +8,13 @@ class cartPage extends Component {
     const products = JSON.parse(localStorage.getItem('products') || '[]');
     return (
       <div data-testid="shopping-cart-empty-message">
-        <Link to="/checkout" data-testid="checkout-products">Checkout</Link>
         {
           products.length > 0
-            ? <CardCart products={ products } />
+            ?
+              <div>
+                 <Link to="/checkout" data-testid="checkout-products">Checkout</Link>
+                 <CardCart products={ products } />
+              </div>
             : <EmptyCart />
         }
       </div>
