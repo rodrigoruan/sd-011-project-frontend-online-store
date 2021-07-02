8f226544af0e990832dc8536a14f507849833f09
Guilherme Pereira de Oliveira <guilhermepdo1508@gmail.com>
2021-07-02 11:43:58 -0300

    Feito css ProductDetails

--- src/pages/ProductDetails.jsx
+++ src/pages/ProductDetails.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 import carrinho from '../images/carrinho.png';
 import RatingForm from '../components/RatingForm';
 import AddToCart from '../components/AddToCart';
+import './ProductDetails.css';
 
 class ProductDetails extends React.Component {
   constructor(props) {
@@ -30,33 +31,44 @@ class ProductDetails extends React.Component {
     const { title, thumbnail, price, attributes, freeShipping } = this.state;
     const { productDetails, quantityIcon, updateQuantityIcon } = this.props;
     return (
-      <div>
-        <p className="productDetailsName" data-testid="product-detail-name">{title}</p>
-        <img src={ thumbnail } alt="Foto do Produto" />
-        <p className="productDetailsPrice">{`Preço: R$${price}`}</p>
-        <ul>
-          {
-            attributes ? attributes.map(({ name, value_name: value }, index) => (
-              <li key={ index }>{`${name}: ${value}`}</li>
-            )) : <p>Loading...</p>
-          }
-        </ul>
-        <p>
-          Frete Grátis:
-          { freeShipping
-            ? <span data-testid="free-shipping">Sim</span>
-            : <span>Não</span> }
-        </p>
-        <AddToCart
-          item={ productDetails }
-          test="product-detail-add-to-cart"
-          updateQuantityIcon={ updateQuantityIcon }
-        />
-        <Link data-testid="shopping-cart-button" to="/shopping-cart">
-          <img src={ carrinho } alt="carrinho" />
-          <span data-testid="shopping-cart-size">{ quantityIcon }</span>
-        </Link>
-        <div>
+      <div className="product-details-page">
+        <div className="product-details-header">
+          <div className="home-link">
+            <Link to="/"><button className="home" type="button">Home</button></Link>
+          </div>
+          <Link data-testid="shopping-cart-button" to="/shopping-cart">
+            <img className="cart-image" src={ carrinho } alt="carrinho" />
+            <span className="cart-number" data-testid="shopping-cart-size">
+              { quantityIcon }
+            </span>
+          </Link>
+        </div>
+        <div className="product-details-container">
+          <div className="product-details-main">
+            <p data-testid="product-detail-name" className="product-detail-name">
+              {title}
+            </p>
+            <img src={ thumbnail } alt="Foto do Produto" />
+            <p>{`Preço: R$${price}`}</p>
+            <ul>
+              {
+                attributes ? attributes.map(({ name, value_name: value }, index) => (
+                  <li key={ index }>{`${name}: ${value}`}</li>
+                )) : <p>Loading...</p>
+              }
+            </ul>
+            <p>
+              { 'Frete Grátis: ' }
+              { freeShipping
+                ? <span data-testid="free-shipping">Sim</span>
+                : <span>Não</span> }
+            </p>
+            <AddToCart
+              item={ productDetails }
+              test="product-detail-add-to-cart"
+              updateQuantityIcon={ updateQuantityIcon }
+            />
+          </div>
           <RatingForm />
         </div>
       </div>
