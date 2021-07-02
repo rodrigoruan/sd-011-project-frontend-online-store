bb267e1c079797f4bb9b0e4cb60af28239fb84bf
Guilherme Pereira de Oliveira <guilhermepdo1508@gmail.com>
2021-07-02 11:53:21 -0300

    Feito css RatingForm

--- src/components/RatingForm.jsx
+++ src/components/RatingForm.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './RatingForm.css';
 
 class RatingForm extends Component {
   constructor() {
@@ -27,31 +28,30 @@ class RatingForm extends Component {
     render() {
       const { comment, prevComments } = this.state;
       return (
-        <div>
+        <div className="rating-form-container">
           <div>
             <h3>Avaliações</h3>
             {prevComments.map((comments, index) => (
               <p key={ index }>{`Comentário: ${comments}`}</p>
             ))}
           </div>
-          <form>
-            <input placeholder="E-mail" type="text" name="email" />
+          <form className="rating-form">
+            <input className="email" placeholder="E-mail" type="text" name="email" />
             <input
               type="number"
               placeholder="Nota"
               min={ 0 }
               max={ 5 }
+              className="rating"
             />
-            <div>
-              <textarea
-                data-testid="product-detail-evaluation"
-                type="text"
-                onChange={ this.currentComment }
-                name="comment"
-                value={ comment }
-                placeholder="Mensagem(opcional)"
-              />
-            </div>
+            <textarea
+              data-testid="product-detail-evaluation"
+              type="text"
+              onChange={ this.currentComment }
+              name="comment"
+              value={ comment }
+              placeholder="Mensagem(opcional)"
+            />
             <button onClick={ this.sendComment } type="button">Avaliar</button>
           </form>
         </div>
