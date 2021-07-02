90dd4820aa8c16690f15962be32e91f6a77f9b15
Guilherme Pereira de Oliveira <guilhermepdo1508@gmail.com>
2021-07-02 11:19:56 -0300

    Feito css ShoppingCart

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import closeButton from '../images/close-button.png';
+import './ShoppingCart.css';
 
 export default class ShoppingCart extends Component {
   constructor() {
@@ -62,64 +63,75 @@ export default class ShoppingCart extends Component {
     let totalPrice = 0;
     const { items, quantity } = this.state;
     return (
-      <div>
-        {items === null ? (
-          <p data-testid="shopping-cart-empty-message">
-            Seu carrinho está vazio
-          </p>
-        )
-          : items.map(
-            ({ title,
-              thumbnail,
-              price,
-              id,
-              available_quantity: availableQuantity }, index) => {
-              totalPrice += price * quantity[index][id];
-              return (
-                <div key={ index } data-testid="shopping-cart-product-name">
-                  <button type="button" onClick={ this.removeAllItems }>
-                    <img
-                      src={ closeButton }
-                      alt="close button"
+      <div className="shopping-cart">
+        <div className="main">
+          <div className="home-link">
+            <Link to="/"><button className="home" type="button">Home</button></Link>
+          </div>
+          {items === null ? (
+            <p data-testid="shopping-cart-empty-message">
+              Seu carrinho está vazio
+            </p>
+          )
+            : items.map(
+              ({ title,
+                thumbnail,
+                price,
+                id,
+                available_quantity: availableQuantity }, index) => {
+                totalPrice += price * quantity[index][id];
+                return (
+                  <div
+                    key={ index }
+                    data-testid="shopping-cart-product-name"
+                    className="cart-product"
+                  >
+                    <button
+                      className="close-button"
+                      type="button"
+                      onClick={ this.removeAllItems }
+                    >
+                      <img src={ closeButton } alt="close button" id={ id } />
+                    </button>
+                    <img src={ thumbnail } alt="Foto do Produto" />
+                    <p className="cart-product-name">{title}</p>
+                    <button
+                      name={ index }
                       id={ id }
-                    />
-                  </button>
-                  <img src={ thumbnail } alt="Foto do Produto" />
-                  <p>{title}</p>
-                  <button
-                    name={ index }
-                    id={ id }
-                    type="button"
-                    data-testid="product-decrease-quantity"
-                    onClick={ this.decreaseQuantity }
-                  >
-                    -
-                  </button>
-                  <p data-testid="shopping-cart-product-quantity">
-                    {`Quantidade: ${quantity[index][id]}`}
-                  </p>
-                  <button
-                    name={ index }
-                    id={ id }
-                    type="button"
-                    data-testid="product-increase-quantity"
-                    onClick={
-                      (event) => this.increaseQuantity(event, availableQuantity)
-                    }
-                  >
-                    +
-                  </button>
-                  <p>{`Preço: R$${price}`}</p>
-                </div>
-              );
-            },
-          )}
-        <p>{`Valor total: R$${totalPrice.toFixed(2)}`}</p>
-        <Link to="/checkout" data-testid="checkout-products">
-          <button type="button">
-            Finalizar compra
-          </button>
-        </Link>
+                      type="button"
+                      data-testid="product-decrease-quantity"
+                      onClick={ this.decreaseQuantity }
+                    >
+                      -
+                    </button>
+                    <p data-testid="shopping-cart-product-quantity">
+                      {`Quantidade: ${quantity[index][id]}`}
+                    </p>
+                    <button
+                      name={ index }
+                      id={ id }
+                      type="button"
+                      data-testid="product-increase-quantity"
+                      onClick={
+                        (event) => this.increaseQuantity(event, availableQuantity)
+                      }
+                    >
+                      +
+                    </button>
+                    <p>{`Preço: R$${price}`}</p>
+                  </div>
+                );
+              },
+            )}
+        </div>
+        <div className="total-price">
+          <p>{`Valor total: R$${totalPrice.toFixed(2)}`}</p>
+          <Link to="/checkout" data-testid="checkout-products">
+            <button type="button">
+              Finalizar compra
+            </button>
+          </Link>
+        </div>
       </div>
     );
   }
