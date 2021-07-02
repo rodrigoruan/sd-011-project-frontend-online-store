01745b2d9b3f85010aa37f3aa99428a993186a4a
Guilherme Pereira de Oliveira <guilhermepdo1508@gmail.com>
2021-07-02 12:08:06 -0300

    Feito css Checkout

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import CheckoutForm from '../components/CheckoutForm';
+import './Checkout.css';
 
 class Checkout extends React.Component {
   constructor() {
@@ -20,29 +22,40 @@ class Checkout extends React.Component {
     let totalPrice = 0;
     const { items, quantity } = this.state;
     return (
-      <div>
-        {items === null ? (
-          <p data-testid="shopping-cart-empty-message">
-            Seu carrinho está vazio
-          </p>
-        )
-          : items.map(
-            ({ title, thumbnail, price, id }, index) => {
-              totalPrice += price * quantity[index][id];
-              return (
-                <div key={ index } data-testid="shopping-cart-product-name">
-                  <img src={ thumbnail } alt="Foto do Produto" />
-                  <p>{title}</p>
-                  <p>
-                    {`Quantidade: ${quantity[index][id]}`}
-                  </p>
-                  <p>{`Preço: R$${price}`}</p>
-                </div>
-              );
-            },
-          )}
-        <p>{`Valor total: R$${totalPrice.toFixed(2)}`}</p>
-        <CheckoutForm />
+      <div className="checkout-page">
+        <div className="main">
+          <div className="home-link">
+            <Link to="/"><button className="home" type="button">Home</button></Link>
+          </div>
+          {items === null ? (
+            <p data-testid="shopping-cart-empty-message">
+              Seu carrinho está vazio
+            </p>
+          )
+            : items.map(
+              ({ title, thumbnail, price, id }, index) => {
+                totalPrice += price * quantity[index][id];
+                return (
+                  <div
+                    className="cart-product"
+                    key={ index }
+                    data-testid="shopping-cart-product-name"
+                  >
+                    <img src={ thumbnail } alt="Foto do Produto" />
+                    <p className="cart-product-name">{title}</p>
+                    <p>
+                      {`Quantidade: ${quantity[index][id]}`}
+                    </p>
+                    <p>{`Preço: R$${price}`}</p>
+                  </div>
+                );
+              },
+            )}
+        </div>
+        <div className="last-step">
+          <p>{`Valor total: R$${totalPrice.toFixed(2)}`}</p>
+          <CheckoutForm />
+        </div>
       </div>
     );
   }
