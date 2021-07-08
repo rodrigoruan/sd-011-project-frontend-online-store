a5c4dda74b7bde6d2b9743608f63e895888e325a
Luana Souza <luanah.souzza@hotmail.com>
2021-07-07 21:09:43 -0300

    Corrige erros Linter

--- src/App.js
+++ src/App.js
@@ -1,66 +1,90 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 import ShoppingCart from './pages/ShoppingCart';
 import ProductDetails from './pages/ProductDetails';
 import Checkout from './pages/Checkout';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      cartItems: {},
-      totalCount: 0,
-    };
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = { cartList: {} };
   }
 
-  componentDidMount() {
-    this.getLocalStorage();
-  }
+  addItemToCart = (event) => {
+    const { cartList } = this.state;
 
-  addToCart = (id, productInfo) => {
-    this.setState(
-      ({ cartItems }) => ({
-        cartItems: { ...cartItems, [id]: productInfo },
-      }),
-      () => {
-        this.sumTotalProducts();
-        this.setLocalStorage();
-      },
-    );
-  };
+    const clickedItemId = event.target.getAttribute('data-id');
+    const clickedItemTitle = event.target.getAttribute('data-title');
+    const clickedItemPrice = event.target.getAttribute('data-price');
+    const clickedItemThumbnail = event.target.getAttribute('data-thumbnail');
+    const clickedItemMaxAvailable = event.target.getAttribute('data-available-quantity');
 
-  setLocalStorage = () => {
-    const { cartItems } = this.state;
-    localStorage.setItem('cartItems', JSON.stringify(cartItems));
-  };
+    const currentItemsinCart = Object.keys(cartList);
 
-  getLocalStorage = () => {
-    const cartItems = JSON.parse(localStorage.getItem('cartItems'));
-    if (cartItems) {
-      this.setState({ cartItems });
+    if (!currentItemsinCart.includes(clickedItemId)) {
+      this.setState((prevState) => ({
+        cartList: Object.assign(
+          prevState.cartList,
+          { [clickedItemId]: {
+            qty: 1,
+            title: clickedItemTitle,
+            price: clickedItemPrice,
+            thumbnail: clickedItemThumbnail,
+            maxAvailable: clickedItemMaxAvailable } },
+        ),
+      }));
+    } else {
+      const updatedCart = { ...cartList };
+      updatedCart[clickedItemId].qty += 1; // Colocar IF do MAX
+      this.setState({ cartList: updatedCart }, () => this.saveCartToLocalStorage());
     }
-  };
+  }
 
-  sumTotalProducts = () => {
-    const { cartItems } = this.state;
-    const totalCount = Object.values(cartItems).reduce(
-      (a, b) => a + b.amount,
-      0,
-    );
-    this.setState({ totalCount });
-  };
+  updateItemQtyInCart = (event) => {
+    const operation = event.target.getAttribute('operation');
+    const itemId = event.target.getAttribute('data-id');
+    // const itemMax = event.target.getAttribute('data-available-quantity');
+    const { cartList } = this.state;
+    const updatedCart = { ...cartList };
 
-  freeShipping = (product) => {
-    const { shipping } = product;
-    const freeShipping = shipping.free_shipping;
-    if (freeShipping) return (<div data-testid="free-shipping">Frete Grátis</div>);
+    if (operation === '+' && updatedCart[itemId].qty < updatedCart[itemId].maxAvailable) {
+      updatedCart[itemId].qty += 1;
+    } else if (operation === '-' && updatedCart[itemId].qty > 1) {
+      updatedCart[itemId].qty -= 1; // Futura função para remover o item do carrinho
+      // } else {
+      //   updatedCart[itemId].qty -= 1;
+    }
+
+    this.setState({ cartList: updatedCart }, () => this.saveCartToLocalStorage());
+  }
+
+  returnTotalCartItemQty = () => {
+    const { cartList } = this.state;
+    return Object.values(cartList).reduce((accum, currItem) => accum + currItem.qty, 0);
+  }
+
+  saveCartToLocalStorage = () => {
+    const { cartList } = this.state;
+    localStorage.setItem('cartList', JSON.stringify(cartList));
+  }
+
+  pushLocalStoragetoState = () => {
+    const localStorageCartList = JSON.parse(localStorage.getItem('cartList'));
+    this.setState({ cartList: { ...localStorageCartList } });
+  }
+
+  componentDidMount = () => {
+    this.pushLocalStoragetoState();
+  }
+
+  componentWillUnmount = () => {
+    this.saveCartToLocalStorage();
   }
 
   render() {
-    const { cartItems, totalCount } = this.state;
+    const { cartList } = this.state;
 
     return (
       <BrowserRouter>
@@ -68,37 +92,43 @@ class App extends Component {
           <Route
             exact
             path="/"
-            render={ () => (
+            render={ (props) => (
               <Home
-                addToCart={ this.addToCart }
-                cartItems={ cartItems }
-                totalCount={ totalCount }
-                freeShipping={ this.freeShipping }
+                { ...props }
+                addItemToCart={ this.addItemToCart }
+                totalItemQty={ this.returnTotalCartItemQty() }
               />
             ) }
           />
           <Route
-            path="/shoppingcart"
-            render={ () => (
+            exact
+            path="/shopping-cart"
+            render={ (props) => (
               <ShoppingCart
-                setLocalStorage={ this.setLocalStorage }
-                cartItems={ cartItems }
+                { ...props }
+                cartList={ cartList }
+                updateItemQtyInCart={ this.updateItemQtyInCart }
               />
             ) }
           />
           <Route
-            path="/checkout"
-            render={ () => <Checkout cartItems={ cartItems } /> }
-          />
-          <Route
-            path="/:title"
+            exact
+            path="/products/:id"
             render={ (props) => (
               <ProductDetails
-                addToCart={ this.addToCart }
-                cartItems={ cartItems }
-                totalCount={ totalCount }
-                freeShipping={ this.freeShipping }
                 { ...props }
+                addItemToCart={ this.addItemToCart }
+                totalItemQty={ this.returnTotalCartItemQty() }
+              />
+            ) }
+          />
+          <Route
+            exact
+            path="/checkout"
+            render={ (props) => (
+              <Checkout
+                { ...props }
+                cartList={ cartList }
               />
             ) }
           />
